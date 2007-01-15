22fddcdf153cd7c1f412b662ae45cebafc19f576
synic <>
2007-01-15 23:33:38 +0000

    committing media directory

---
+++ xl/media/flac.py
@@ -0,0 +1,36 @@
+import mutagen.flac
+
+def get_tag(flac, tag):
+    try:    
+        return unicode(flac[tag][0])
+    except KeyError:
+        return ''
+
+def fill_tag_from_path(tr):
+    """
+        Reads all tags from the file
+    """
+    f = mutagen.flac.FLAC(tr.loc)
+    tr.length = int(f.info.length)
+
+    tr.artist = get_tag(f, "artist")
+    tr.album = get_tag(f, "album")
+    tr.track = get_tag(f, "tracknumber")
+    tr.disc_id = get_tag(f, 'tracktotal')
+    tr.title = get_tag(f, "title")
+    tr.genre = get_tag(f, "genre")
+    tr.year = get_tag(f, "date")
+
+
+def write_tag(tr):
+    f = mutagen.flac.FLAC(tr.loc)
+    if f.vc is None: f.add_vorbiscomment()
+    del(f.vc[:])
+    f.vc['artist'] = tr.artist
+    f.vc['album'] = tr.album
+    f.vc['title'] = tr.title
+    f.vc['tracktotal'] = tr.disc_id
+    f.vc['genre'] = tr.genre
+    f.vc['tracknumber'] = str(tr.track)
+    f.vc['date'] = str(tr.year)
+    f.save()
---
+++ xl/media/wma.py
@@ -0,0 +1,19 @@
+import lib.wmainfo
+
+def get_tag(inf, name):
+    if inf.tags.has_key(name):
+        return inf.tags[name]
+    else:
+        return ''
+
+def fill_tag_from_path(tr):
+    inf = lib.wmainfo.WmaInfo(tr.loc)
+
+    tr.length = inf.info["playtime_seconds"]
+    tr.bitrate = inf.info["max_bitrate"]
+    tr.artist = get_tag(inf, 'Author')
+    tr.album = get_tag(inf, 'AlbumTitle')
+    tr.title = get_tag(inf, 'Title') 
+    tr.genre = ""
+    tr.track = get_tag(inf, 'TrackNumber')
+    tr.year = get_tag(inf, 'Year')

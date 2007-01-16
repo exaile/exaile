a1f4b500decb572c187ef32e33dfc1000a60b8de
synic <>
2007-01-16 03:28:06 +0000

    brazilian translation added

--- po/createpot.py
+++ po/createpot.py
@@ -4,6 +4,6 @@ import os, sys
 
 if len(sys.argv) <= 1:
     os.system("intltool-extract --type=gettext/glade exaile.glade")
-    os.system("xgettext -k_ -kN_ -o messages.pot *.py xl/*.py exaile.glade.h")
+    os.system("xgettext -k_ -kN_ -o messages.pot *.py xl/*.py exaile.glade.h plugins/plugins.glade")
     print "Now edit messages.pot, save it as <locale>.po, and send it" \
         " to arolsen@gmail.com.\nThanks!\n"

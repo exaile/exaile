b1b3aacd5c371e9995be40ea8344eb4f7135f40e
sjohannes <>
2007-03-08 04:12:46 +0000

    oops, committed wrong files

--- po/createpot.py
+++ po/createpot.py
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env python
 
-intltool-extract --type=gettext/glade exaile.glade &&
-xgettext -k_ -kN_ -o messages.pot *.py xl/*.py exaile.glade.h \
-    plugins/plugins.glade &&
-echo -e "Now edit messages.pot, save it as <locale>.po, and send it to" \
-    "arolsen@gmail.com.\nThanks!\n"
+import os, sys
+
+if len(sys.argv) <= 1:
+    os.system("intltool-extract --type=gettext/glade exaile.glade")
+    os.system("xgettext -k_ -kN_ -o messages.pot *.py xl/*.py exaile.glade.h plugins/plugins.glade")
+    print "Now edit messages.pot, save it as <locale>.po, and send it" \
+        " to arolsen@gmail.com.\nThanks!\n"

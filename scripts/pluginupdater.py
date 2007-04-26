782b5023501edd9ebffdba0451291cf26f62f9b9
synic <>
2007-04-26 22:48:53 +0000

    renamed to pluginupdater.py

--- scripts/pluginupdater.py
+++ scripts/pluginupdater.py
@@ -4,6 +4,9 @@ import re, os, cgi, time, urllib, sys
 import os.path
 plugin_re = re.compile(r'<a class="file" title="View File" href=".*?">(\w+.py)</a>', re.DOTALL|re.MULTILINE)
 
+if len(sys.argv) != 2:
+    print "usage: python pluginupdater.py [exaile_version]"
+    sys.exit(0)
 version = sys.argv[1]
 data = urllib.urlopen('http://exaile.org/trac/browser/plugins/%s' %
     version).read()

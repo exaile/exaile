6574823eea30956f3a52705dde4983fa229d1811
synic <>
2007-04-27 15:31:36 +0000

    createpot.py was not getting the plugins glade file

--- po/createpot.py
+++ po/createpot.py
@@ -4,6 +4,7 @@ import os, sys
 
 if len(sys.argv) <= 1:
     os.system("intltool-extract --type=gettext/glade exaile.glade")
-    os.system("xgettext -k_ -kN_ -o messages.pot *.py xl/*.py exaile.glade.h plugins/plugins.glade")
+    os.system("intltool-extract --type=gettext/glade plugins/plugins.glade")
+    os.system("xgettext -k_ -kN_ -o messages.pot *.py xl/*.py exaile.glade.h plugins/plugins.glade.h")
     print "Now edit messages.pot, save it as <locale>.po, and send it" \
         " to arolsen@gmail.com.\nThanks!\n"

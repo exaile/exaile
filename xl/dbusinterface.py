2a9206d95743364a67f7f2481a4319f31bdf54be
sjohannes <>
2007-04-28 05:17:14 +0000

    xl/dbusinterface.py: Ouch, None is not an INT32.

--- xl/dbusinterface.py
+++ xl/dbusinterface.py
@@ -213,7 +213,7 @@ class DBusInterfaceObject(dbus.service.Object):
 
     @dbus.service.method("org.exaile.DBusInterface", None, "i")
     def get_rating(self):
-        if not self.exaile.player.current: return None
+        if not self.exaile.player.current: return -1
         return self.exaile.player.current._rating
 
 

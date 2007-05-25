4f30d8d6f5fa7643bc948d9a77cd5b5ce95ff120
sjohannes <>
2007-05-25 12:18:02 +0000

    Fixed dumb syntax error I made in previous rev.

--- xl/panels.py
+++ xl/panels.py
@@ -1694,7 +1694,7 @@ class PRadioPanel(object):
         tr.type = 'lastfm'
         tr.track = -1
         tr.title = station
-        tr.album = _("%(user)s's Last.fm %(station)s") %
+        tr.album = _("%(user)s's Last.fm %(station)s") % \
             {
                 'user': user,
                 'station': station

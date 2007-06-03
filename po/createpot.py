5dbfbb7d635890ba32f5854fe339ab2db92df518
synic <>
2007-06-03 18:19:12 +0000

    sorry, plugins/*.py

--- po/createpot.py
+++ po/createpot.py
@@ -5,7 +5,7 @@ import os, sys
 if len(sys.argv) <= 1:
     os.system("intltool-extract --type=gettext/glade exaile.glade")
     os.system("intltool-extract --type=gettext/glade plugins/plugins.glade")
-    os.system("xgettext -k_ -kN_ -o messages.pot *.py xl/plugins/*.py xl/media/*.py xl/*.py exaile.glade.h plugins/plugins.glade.h")
+    os.system("xgettext -k_ -kN_ -o messages.pot *.py plugins/*.py xl/media/*.py xl/*.py exaile.glade.h plugins/plugins.glade.h")
     print "\n\n**********\n"
     print "Now edit messages.pot, save it as <locale>.po, and post it on\n" \
         "our ticket tracker (http://www.exaile.org/newtranslation)\n" \

058c47cfbaf07bdcff9a510322612145b8de8662
reacocard <>
2007-06-04 20:14:07 +0000

    Fix for #555.

--- exaile.py
+++ exaile.py
@@ -1836,7 +1836,7 @@ class ExaileWindow(gobject.GObject):
         frac_value = value / 100.0
         self.player.set_volume(frac_value)
         self.settings['volume'] = frac_value
-        if not self.window.get_property('visible') and  self.settings.get_boolean("osd/enabled", True):
+        if not self.window.has_toplevel_focus() and self.settings.get_boolean("osd/enabled", True):
             pop = xlmisc.get_osd(self, xlmisc.get_osd_settings(self.settings))
             vol_text = "<big><b> " + _("Changing volume: %d %%") % \
                 self.get_volume_percent() + "</b></big>"

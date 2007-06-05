bfd3eb0c20051342e8bb7981690369cdede7d0b9
synic <>
2007-06-05 14:50:34 +0000

    updating createpot.po so that if you pass 'compile' as the first parameter, it will merge all the po files in po/ with a current messages.pot, and will msgformat them all into their corresponding .mo files

--- po/createpot.py
+++ po/createpot.py
@@ -2,11 +2,35 @@
 
 import os, sys
 
-if len(sys.argv) <= 1:
-    os.system("intltool-extract --type=gettext/glade exaile.glade")
-    os.system("intltool-extract --type=gettext/glade plugins/plugins.glade")
-    os.system("xgettext -k_ -kN_ -o messages.pot *.py plugins/*.py xl/media/*.py xl/*.py exaile.glade.h plugins/plugins.glade.h")
+command = None
+try:
+    command = sys.argv[1]
+except IndexError:
+    pass
+
+os.system("intltool-extract --type=gettext/glade exaile.glade")
+os.system("intltool-extract --type=gettext/glade plugins/plugins.glade")
+os.system("xgettext -k_ -kN_ -o messages.pot *.py plugins/*.py xl/media/*.py xl/*.py exaile.glade.h plugins/plugins.glade.h")
+
+if command != 'compile':
     print "\n\n**********\n"
     print "Now edit messages.pot, save it as <locale>.po, and post it on\n" \
         "our ticket tracker (http://www.exaile.org/newtranslation)\n" \
         "as a new translation.\n\nThank you!"
+
+else:
+    os.chdir('po')
+
+    files = os.listdir('.')
+    for f in files:
+        if f.endswith('.po'):
+            d = f.replace('.po', '')
+            if not os.path.isdir(d):
+                os.mkdir(d, 0777)
+            if not os.path.isdir('%s/LC_MESSAGES' % d):
+                os.mkdir('%s/LC_MESSAGES' % d, 0777)
+
+            os.system('msgmerge -U %s ../messages.pot' % f)
+            os.system('msgfmt %s -o %s/LC_MESSAGES/exaile.mo' % (f, d))
+
+    os.chdir('..')

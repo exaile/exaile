fedcf8eb9b4004e8dcf3d9d774cd8c68e77a7f9a
synic <>
2007-07-02 20:03:07 +0000

    Fixing the ipoddriver and the device panel

--- xl/panels/device.py
+++ xl/panels/device.py
@@ -14,7 +14,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-import gtk
+import gtk, urllib, re, gobject
 from xl import common, xlmisc, library
 from gettext import gettext as _
 import collection

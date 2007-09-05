684bb3e7a4231c6b8bd2bd0894c99025251cfaff
Johannes Sasongko <sasongko@gmail.com>
2007-09-06 05:01:56 +1000

    plugins/shoutcast: Up revision for previous change.

--- plugins/shoutcast.py
+++ plugins/shoutcast.py
@@ -25,7 +25,7 @@ import xl.plugins as plugins
 PLUGIN_ID = 'Shoutcast Radio'
 PLUGIN_NAME = _("Shoutcast Radio")
 PLUGIN_AUTHORS = ['Adam Olsen <arolsen@gmail.com>']
-PLUGIN_VERSION = '0.4.3'
+PLUGIN_VERSION = '0.4.4'
 PLUGIN_DESCRIPTION = _(r"""Allows you to browse the Shoutcast Streaming Radio
 network""")
 PLUGIN_ENABLED = False

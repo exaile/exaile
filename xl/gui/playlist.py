55de9652c158de5b112cc0c3c93aae0f51dc6a89
Adam Olsen <arolsen@gmail.com>
2007-09-26 09:40:12 -0600

    oops

--- xl/gui/playlist.py
+++ xl/gui/playlist.py
@@ -32,9 +32,10 @@ def create_rating_images(caller):
         caller.rating_images = []
         star_size = caller.rating_width / 4
 
-        star_size -= 1
         star = gtk.gdk.pixbuf_new_from_file_at_size(
             xl.path.get_data('images', 'star.png'), star_size, star_size)
+
+        star_size -= 1
 
         full_image = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, True, 8, caller.rating_width, star_size)
         full_image.fill(0xffffff00) # transparent white

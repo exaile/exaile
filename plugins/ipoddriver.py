1e50a42137f2d553173aa4836e99cb2eb0d5b38d
Johannes Sasongko <sasongko@gmail.com>
2007-10-15 01:32:48 +1000

    iPod: Fix cannot remove songs. Also speed it up a bit.

--- plugins/ipoddriver.py
+++ plugins/ipoddriver.py
@@ -24,7 +24,7 @@ import xl.path
 
 PLUGIN_NAME = _("iPod Device Driver")
 PLUGIN_AUTHORS = ['Adam Olsen <arolsen@gmail.com>']
-PLUGIN_VERSION = '0.4.5'
+PLUGIN_VERSION = '0.4.6'
 PLUGIN_DESCRIPTION = _(r"""iPod Driver for the Devices Panel
 \n\nRequires libgpod and its Python bindings to be installed
 \n(e.g. python-gpod in Debian).""")
@@ -148,9 +148,11 @@ class iPodDriver(plugins.DeviceDriver):
 
             gpod.itdb_track_unlink(track)
             self.all.remove(song)
-            for k, v in self.lists.iteritems():
-                if track.loc in v:
-                    self.lists[k].remove(track.loc)
+            for v in self.lists.itervalues():
+                try:
+                    v.remove(song.loc)
+                except ValueError:
+                    pass
 
         self.transfer_done()
 

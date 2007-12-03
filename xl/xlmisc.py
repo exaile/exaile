067853301f200e93c2c9f9bc768e2f2e5f5d2bc9
Johannes Sasongko <sasongko@gmail.com>
2007-12-03 16:37:47 +1000

    Replace httplib with urllib2 so it supports http_proxy.
    Patch by Marc Poulhiès <marc.poulhies imag fr> from LP bug 135946 comments 23 and 24.

--- xl/xlmisc.py
+++ xl/xlmisc.py
@@ -20,7 +20,7 @@ This file contains every miscellanious dialog and class that is not over
 their own file
 """
 
-import httplib, os, re, sys, threading, time, traceback, urllib, urlparse
+import urllib2, os, re, sys, threading, time, traceback, urllib, urlparse
 from gettext import gettext as _
 
 import pygtk, locale
@@ -692,16 +692,16 @@ class URLFetcher(threading.Thread):
         """
             Called by thread.start()
         """
-
-        conn = httplib.HTTPConnection(self.server)
-
-        conn.request("GET", self.path)
-        response = conn.getresponse()
-
-        if response.status != 200:
+        try:
+            response = urllib2.urlopen("http://" + self.server + self.path)
+        except urllib2.URLError, e:
             self.notify("Could not load page. "
-                "Error %d: %s" % 
-                (response.status, response.reason))
+                "Error: %s" % e.reason)
+            return
+
+        except urllib2.HTTPError, e:
+            self.notify("Could not load page. "
+                "Error %d" % e.status)
             return
 
         page = response.read()

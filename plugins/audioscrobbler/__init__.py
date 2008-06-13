e08f0ec6c990a6ba051c0fbe6e1946c6642fa97e
Aren Olson <reacocard@gmail.com>
2008-06-13 12:09:41 -0500

    add slight delay in audioscobbler's now playing so that skipping tracks doesn't cause multiple submissions

--- plugins/audioscrobbler/__init__.py
+++ plugins/audioscrobbler/__init__.py
@@ -100,7 +100,11 @@ class ExaileScrobbler(object):
         if self.time_started == "":
             self.time_started = 0
         self.start_time = time.time()
-        self.now_playing(track)
+
+        # wait 5 seconds before now playing to allow for skipping
+        time.sleep(5)
+        if self.player.current == track:
+            self.now_playing(track)
 
     def on_stop(self, type, player, track):
         if not track: 

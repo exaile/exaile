fdf37622f9070369e7e507d8a26e5e880aac167b
Adam Olsen <arolsen@gmail.com>
2008-06-25 17:33:43 -0600

    again ... making tests happy.  I'll get the 'make sure tests work before you commit' thing down sometime

--- tests/gui/base.py
+++ tests/gui/base.py
@@ -3,7 +3,7 @@ from guitest.utils import mainloop_handler
 import pygtk, gtk
 import xlgui
 import tests.base
-from xl import playlist, radio
+from xl import playlist, radio, player
 
 class FakeRadio(object):
     stations = {}
@@ -15,6 +15,8 @@ class BaseTestCase(GtkTestCase,
         self.radio = FakeRadio()
         GtkTestCase.setUp(self)
         tests.base.BaseTestCase.setUp(self)
+        self.player = player.get_player()()
+        self.queue = player.PlayQueue(self.player)
         self.playlists = playlist.PlaylistManager()
         self.radio = radio.RadioManager()
         self.gui = xlgui.Main(self)

ab6b77f06120f4a229ed9241af8f5e8eea71c251
Adam Olsen <arolsen@gmail.com>
2008-07-07 12:39:33 -0600

    lyricsfly patch from Anthony Waters #246155

--- plugins/lyricsfly/__init__.py
+++ plugins/lyricsfly/__init__.py
@@ -73,6 +73,8 @@ class LyricsFly(LyricSearchMethod):
             url = "http://lyricsfly.com/search/view.php?%s&view=%s" % (
                 urllib.quote_plus(cs),
                 urllib.quote_plus(id)) 
+            #Take out the [br]
+            lyrics = lyrics.replace("[br]","\n")
             return (lyrics, url)
         else:
             raise LyricsNotFoundException

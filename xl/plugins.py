809aa4f79a494640c910823d76e4f1b8db589478
Adam Olsen <arolsen@gmail.com>
2008-10-17 17:44:36 -0600

    removing console plugin as a default plugin

--- xl/plugins.py
+++ xl/plugins.py
@@ -20,7 +20,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 # list of plugins to enable by default on new installs
-DEFAULT_PLUGINS = ['shoutcast', 'amazoncovers', 'console', 'lastfmcovers']
+DEFAULT_PLUGINS = ['shoutcast', 'amazoncovers', 'lastfmcovers']
 
 class InvalidPluginError(Exception):
     pass

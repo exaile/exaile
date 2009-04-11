53dbe5853b82fdd72c4e3740daf774677fa98546
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2009-04-11 18:09:53 -0400

    plugins/mpris/test.py: Added docstrings, fixed testPosition

--- plugins/mpris/test.py
+++ plugins/mpris/test.py
@@ -1,3 +1,7 @@
+"""
+Simple test case for MPRIS. Make sure you set the global variable FILE with an
+absolute path to a valid playable, local music piece before running this test
+"""
 import unittest
 import dbus
 import os
@@ -5,15 +9,25 @@ import time
 
 OBJECT_NAME = 'org.mpris.exaile'
 INTERFACE = 'org.freedesktop.MediaPlayer'
-FILE = os.path.expanduser(os.path.join("~", "Desktop",
-                                    "01 Seven Nation Army.mp3"))
-assert os.path.isfile(FILE)
+FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.path.pardir, os.path.pardir,
+                    'tests', 'data', 'music', 'delerium', 'chimera',
+                    '05 - Truly.mp3')
+assert os.path.isfile(FILE), FILE + " must be a valid musical piece"
 FILE = "file://" + FILE
 print "Test file will be: " + FILE
 
 class TestExaileMpris(unittest.TestCase):
 
+    """
+        Tests Exaile MPRIS plugin
+    """
+
     def setUp(self):
+        """
+            Simple setUp that makes dbus connections and assigns them to
+            self.player, self.track_list, and self.root. Also begins playing
+            a song so every test case can assume that a song is playing
+        """
         bus = dbus.SessionBus()
         objects = {'root': '/',
                    'player': '/Player',
@@ -30,6 +44,9 @@ class TestExaileMpris(unittest.TestCase):
         time.sleep(0.5)
 
     def __wait_after(function):
+        """
+            Decorator to add a delay after a function call
+        """
         def inner(*args, **kwargs):
             function(*args, **kwargs)
             time.sleep(0.5)
@@ -37,35 +54,68 @@ class TestExaileMpris(unittest.TestCase):
 
     @__wait_after
     def _stop(self):
+        """
+            Stops playing w/ delay
+        """
         self.player.Stop()
 
     @__wait_after
     def _play(self):
+        """
+            Starts playing w/ delay
+        """
         self.player.Play()
 
     @__wait_after
     def _pause(self):
+        """
+            Pauses playing w/ delay
+        """
         self.player.Pause()
 
 
 class TestMprisRoot(TestExaileMpris):
+
+    """
+        Check / (Root) object functions for MPRIS. Does not check Quit
+    """
+
     def testIdentity(self):
+        """
+            Make sure we output Exaile with our identity
+        """
         id = self.root.Identity()
         self.assertEqual(id, self.root.Identity())
         self.assertTrue(id.startswith("Exaile"))
 
     def testMprisVersion(self):
+        """
+            Checks that we are using MPRIS version 1.0
+        """
         version = self.root.MprisVersion()
         self.assertEqual(dbus.UInt16(1), version[0])
         self.assertEqual(dbus.UInt16(0), version[1])
 
 class TestTrackList(TestExaileMpris):
+
+    """
+        Tests the /TrackList object for MPRIS
+    """
+
     def testGetMetadata(self):
+        """
+            Make sure we can get metadata. Also makes sure that locations will
+            not change randomly
+        """
         md = self.track_list.GetMetadata(0)
         md_2 = self.track_list.GetMetadata(0)
         self.assertEqual(md, md_2)
 
     def testAppendDelWithoutPlay(self):
+        """
+            Tests appending and deleting songs from the playlist without
+            playing them
+        """
         cur_track = self.track_list.GetCurrentTrack()
         len = self.track_list.GetLength()
 
@@ -81,6 +131,9 @@ class TestTrackList(TestExaileMpris):
         self.assertEqual(cur_track, self.track_list.GetCurrentTrack())
 
     def testAppendDelWithPlay(self):
+        """
+            Tests appending songs into the playlist with playing the songs
+        """
         cur_track = self.track_list.GetCurrentTrack()
         cur_md = self.track_list.GetMetadata(cur_track)
         len = self.track_list.GetLength()
@@ -98,6 +151,9 @@ class TestTrackList(TestExaileMpris):
         self.track_list.AddTrack(cur_md['location'], True)
 
     def testGetCurrentTrack(self):
+        """
+            Check the GetCurrentTrack information
+        """
         cur_track = self.track_list.GetCurrentTrack()
         self.assertTrue(cur_track >= 0, "Tests start with playing music")
 
@@ -110,6 +166,9 @@ class TestTrackList(TestExaileMpris):
                 "After a stop and play, we should be at the same track")
 
     def __test_bools(self, getter, setter):
+        """
+            Generic function for checking that a boolean value changes
+        """
         cur_val = getter()
         if cur_val == dbus.Int32(0):
             val = False
@@ -130,15 +189,29 @@ class TestTrackList(TestExaileMpris):
         self.track_list.SetLoop(val)
 
     def testLoop(self):
+        """
+            Tests that you can change the loop settings
+        """
         self.__test_bools(lambda: self.player.GetStatus()[3],
                         lambda x: self.track_list.SetLoop(x))
 
     def testRandom(self):
+        """
+            Tests that you can change the random settings
+        """
         self.__test_bools(lambda: self.player.GetStatus()[1],
                         lambda x: self.track_list.SetRandom(x))
 
 class TestPlayer(TestExaileMpris):
+
+    """
+        Tests the /Player object for MPRIS
+    """
+
     def testNextPrev(self):
+        """
+            Make sure you can skip back and forward
+        """
         cur_track = self.track_list.GetCurrentTrack()
         self.player.Next()
         new_track = self.track_list.GetCurrentTrack()
@@ -147,6 +220,9 @@ class TestPlayer(TestExaileMpris):
         self.assertEqual(cur_track, self.track_list.GetCurrentTrack())
 
     def testStopPlayPause(self):
+        """
+            Make sure play, pause, and stop behaive as designed
+        """
         self._stop()
         self.assertEqual(dbus.Int32(2), self.player.GetStatus()[0])
 
@@ -166,6 +242,9 @@ class TestPlayer(TestExaileMpris):
         self.assertEqual(dbus.Int32(2), self.player.GetStatus()[0])
 
     def testVolume(self):
+        """
+            Test to make sure volumes are set happily
+        """
         vol = self.player.VolumeGet()
         self.player.VolumeSet(1 - vol)
         self.assertEqual(1 - vol, self.player.VolumeGet())
@@ -173,25 +252,33 @@ class TestPlayer(TestExaileMpris):
         self.assertEqual(vol, self.player.VolumeGet())
 
     def testPosition(self):
+        """
+            Test the PositionGet and PositionSet functions. Unfortuantely this
+            is very time sensitive and thus has about a 10 second sleep in the
+            function
+        """
+        time.sleep(3)
         self._pause()
-        time.sleep(1)
 
         pos = self.player.PositionGet()
-        time.sleep(0.5)
+        time.sleep(1)
         self.assertEqual(pos, self.player.PositionGet(),
                 "Position shouldn't move while paused")
 
         self._pause()
-        time.sleep(1)
-        new_pos = self.player.PositionGet()
-        self.assertTrue(pos < new_pos,
+        time.sleep(4)
+        last_pos = self.player.PositionGet()
+        self.assertTrue(pos < last_pos,
                 "Position shouldn't advance while paused: %d >= %d" %
-                    (pos, new_pos))
+                    (pos, last_pos))
 
-        self._pause()
         self.player.PositionSet(pos)
-        self.assertEqual(pos, self.player.PositionGet(),
-                "Set position back to start")
+        time.sleep(2)
+        self._pause()
+        mid_pos = self.player.PositionGet(),
+        self.assertTrue(mid_pos[0] < last_pos,
+                "Resetting to position %d, %d should be between that at %d"
+                         % (pos, mid_pos[0], last_pos))
 
         self._pause()
         time.sleep(0.5)
@@ -199,7 +286,7 @@ class TestPlayer(TestExaileMpris):
                 "Make sure it still advances")
 
         self.player.PositionSet(-1)
-        self.assertEqual(pos, self.player.PositionGet(),
+        self.assertTrue(pos < self.player.PositionGet(),
                 "Don't move to invalid position")
         
 

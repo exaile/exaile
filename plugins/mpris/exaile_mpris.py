923f8c2710ffa42e3dca4b495d9f5c8218a5ba9f
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2009-04-11 18:08:41 -0400

    Added Docstrings to exaile_mpris

--- plugins/mpris/exaile_mpris.py
+++ plugins/mpris/exaile_mpris.py
@@ -21,6 +21,9 @@ class ExaileMpris(object):
     """
 
     def __init__(self, exaile=None):
+        """
+            Constructs an MPRIS controller. Note, you must call acquire()
+        """
         self.exaile = exaile
         self.mpris_root = None
         self.mpris_tracklist = None
@@ -28,6 +31,9 @@ class ExaileMpris(object):
         self.bus = None
 
     def release(self):
+        """
+            Releases all objects from D-Bus and unregisters the bus
+        """
         for obj in (self.mpris_root, self.mpris_tracklist, self.mpris_player):
             if obj is not None:
                 obj.remove_from_connection()
@@ -38,16 +44,25 @@ class ExaileMpris(object):
             self.bus.get_bus().release_name(self.bus.get_name())
 
     def acquire(self):
+        """
+            Connects to D-Bus and registers all components
+        """
         self._acquire_bus()
         self._add_interfaces()
 
     def _acquire_bus(self):
+        """
+            Connect to D-Bus and set self.bus to be a valid connection
+        """
         if self.bus is not None:
             self.bus.get_bus().request_name(OBJECT_NAME)
         else:
             self.bus = dbus.service.BusName(OBJECT_NAME, bus=dbus.SessionBus())
 
     def _add_interfaces(self):
+        """
+            Connects all interfaces to D-Bus
+        """
         self.mpris_root = mpris_root.ExaileMprisRoot(self.exaile, self.bus)
         self.mpris_tracklist = mpris_tracklist.ExaileMprisTrackList(
                                                         self.exaile, 

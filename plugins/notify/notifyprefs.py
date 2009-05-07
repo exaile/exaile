4dc829915623a20129f290e5663b619d096457a2
papag <papag@mukhea2>
2009-05-07 16:12:16 -0400

    Forgot to add the important parts

---
+++ plugins/notify/notifyprefs.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2009 Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 1, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+import os
+from xlgui.prefs import widgets
+from xl import xdg
+
+name = 'Notify'
+basedir = os.path.dirname(os.path.realpath(__file__))
+glade = os.path.join(basedir, "notifprefs_pane.glade")
+
+class ResizeCovers(widgets.CheckPrefsItem):
+    default = True
+    name = 'plugin/notify/resize'
+

69d1a9804b02f9febb36bfa7c4db1738d93586a5
Mathias Brodala <info@noctus.net>
2009-07-16 14:15:10 +0200

    * Properly name menus in gladefile * Update plugins to reflect the changes

--- plugins/ipconsole/__init__.py
+++ plugins/ipconsole/__init__.py
@@ -125,8 +125,7 @@ def _enable(exaile):
     MENU_ITEM = gtk.MenuItem(_('Show IPython Console'))
     MENU_ITEM.connect('activate', show_console,exaile)
 
-    # tools menu, not renamed yet :b
-    exaile.gui.xml.get_widget('menuitem5').get_submenu().append(MENU_ITEM)
+    exaile.gui.xml.get_widget('tools_menu').get_submenu().append(MENU_ITEM)
     MENU_ITEM.show()
 #    return True    # bad! crashes compiz
 

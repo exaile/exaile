c78122e0d153d66995655d34e76e1fdf74eeb3b0
Johannes Sasongko <sasongko@gmail.com>
2009-07-29 02:05:24 +1000

    Revert more.

--- xlgui/playlist.py
+++ xlgui/playlist.py
@@ -678,7 +678,7 @@ class Playlist(gtk.VBox):
             iter = self.model.get_iter(path)
             song = self.model.get_value(iter, 0)
 
-            uri = urllib.quote(song.get_loc().encode("utf-8"))
+            uri = song.get_loc().encode("utf-8")
             guiutil.DragTreeView.dragged_data[song.get_loc()] = song
             loc.append(uri)
 

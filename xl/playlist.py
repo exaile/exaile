4aa15c820edde915ed6493ef8ac7ce6a5592f588
Aren Olson <reacocard@gmail.com>
2009-07-30 00:55:05 -0500

    fixing a bug with playlist track removal and position updating

--- xl/playlist.py
+++ xl/playlist.py
@@ -538,7 +538,7 @@ class Playlist(object):
         if end < self.current_pos:
             self.current_pos -= len(removed)
         elif start <= self.current_pos <= end:
-            self.current_pos = start+1
+            self.current_pos = start-1
 
         event.log_event('tracks_removed', self, (start, end, removed))
 

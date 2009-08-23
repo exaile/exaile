92afb00caf55a1dad43b9487113ebe9cb30ea1e1
Adam Olsen <arolsen@gmail.com>
2009-08-23 08:50:54 -0600

    amazon cover search works again.  You are just required to enter your AWS api key and secret key in the amazon cover preferences panel.

--- plugins/amazoncovers/__init__.py
+++ plugins/amazoncovers/__init__.py
@@ -1,9 +1,30 @@
+# Copyright (C) 2006 Adam Olsen
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 1, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 import _ecs as ecs
+import amazonprefs
 import urllib, hashlib, time
 from xl.cover import *
 from xl import common, event, metadata
+from xl import settings
+from xl.nls import gettext as _
+import logging
 
-AMAZON_KEY = "15VDQG80MCS2K1W2VRR2" # Adam Olsen's key
+logger = logging.getLogger(__name__)
+
 def enable(exaile):
     if exaile.loading:
         event.add_callback(_enable, "exaile_loaded")
@@ -12,11 +33,14 @@ def enable(exaile):
 
 def _enable(eventname, exaile, nothing):
     exaile.covers.add_search_method(
-        AmazonCoverSearch(AMAZON_KEY) 
+        AmazonCoverSearch() 
     )
 
 def disable(exaile):
     exaile.covers.remove_search_method_by_name('amazon')
+
+def get_prefs_pane():
+    return amazonprefs
 
 class AmazonCoverSearch(CoverSearchMethod):
     """
@@ -24,8 +48,7 @@ class AmazonCoverSearch(CoverSearchMethod):
     """
     name = 'amazon'
     type = 'remote' # fetches remotely as opposed to locally
-    def __init__(self, amazon_key):
-        ecs.setLicenseKey(amazon_key)
+    def __init__(self):
         self.starttime = 0
 
     def find_covers(self, track, limit=-1):
@@ -44,23 +67,31 @@ class AmazonCoverSearch(CoverSearchMethod):
         if waittime > 0: time.sleep(waittime)
 
         self.starttime = time.time()
-        try:
-            albums = ecs.ItemSearch(Keywords=search,
-                SearchIndex="Music",
-                ResponseGroup="ItemAttributes,Images")
-        except ecs.NoExactMatches:
-            raise NoCoverFoundException()
 
+        # get the settings for amazon key and secret key
+        api_key = settings.get_option(
+            'plugin/amazoncovers/api_key', '')
+        secret_key = settings.get_option(
+            'plugin/amazoncovers/secret_key', '')
+
+        if not api_key or not secret_key:
+            logger.warning(_('Please enter your amazon api and secret '
+                'keys in the Amazon Covers Preferences'))
+
+        try:
+            albums = ecs.search_covers(search, api_key, secret_key)
+        except ecs.AmazonSearchError, e:
+            raise NoCoverFoundException(str(e))
 
         covers = []
         for album in albums:
             try:
-                h = urllib.urlopen(album.LargeImage.URL)
+                h = urllib.urlopen(album)
                 data = h.read()
                 h.close()
 
                 covername = os.path.join(cache_dir,
-                    hashlib.md5(album.LargeImage.URL).hexdigest())
+                    hashlib.md5(album).hexdigest())
                 covername += ".jpg"
                 h = open(covername, 'w')
                 h.write(data)
@@ -69,7 +100,8 @@ class AmazonCoverSearch(CoverSearchMethod):
                 covers.append(covername)
                 if limit != -1 and len(covers) >= limit:
                     return covers
-            except AttributeError: continue
+            except AttributeError: 
+                continue
             except:
                 traceback.print_exc()
                 common.log_exception()

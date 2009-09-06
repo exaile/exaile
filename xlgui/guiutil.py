5c3d51dcad7f8c383b13d811875cb160441595e0
Mathias Brodala <info@noctus.net>
2009-09-06 03:04:27 +0200

    * Small cleanups and tweaks

--- xlgui/guiutil.py
+++ xlgui/guiutil.py
@@ -1,7 +1,5 @@
 # Copyright (C) 2008-2009 Adam Olsen 
 #
-# Copyright (C) 2008-2009 Adam Olsen 
-#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2, or (at your option)

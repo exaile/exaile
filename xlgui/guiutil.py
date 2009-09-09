d8706c332cfb57eabeeea6bf26079e1c9073fe78
Mathias Brodala <info@noctus.net>
2009-09-10 00:13:16 +0200

    * Add fallback to remove() in Statusbar for backward compatibility

--- xlgui/guiutil.py
+++ xlgui/guiutil.py
@@ -678,8 +678,12 @@ class Statusbar(object):
         """
             Clears the status message
         """
-        for message_id in self.message_ids:
-            self.status_bar.remove_message(self.context_id, message_id)
+        try:
+            for message_id in self.message_ids:
+                self.status_bar.remove_message(self.context_id, message_id)
+        except AttributeError:
+            for message_id in self.message_ids:
+                self.status_bar.remove(self.context_id, message_id)
 
         del self.message_ids[:]
         self.message_ids = []

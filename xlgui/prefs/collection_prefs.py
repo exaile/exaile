fd294137744ed3baa40412e8bbf4479b29343a85
Thomas Zander <thomas.e.zander@googlemail.com>
2009-10-08 21:49:20 +0200

    Transfer the copyright of xlgui/prefs/collection_prefs.py to Adam Olsen and license it following GPLv2 and later

--- xlgui/prefs/collection_prefs.py
+++ xlgui/prefs/collection_prefs.py
@@ -1,8 +1,8 @@
-# Copyright (C) 2009 Thomas E. Zander
+# Copyright (C) 2009 Adam Olsen
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 1, or (at your option)
+# the Free Software Foundation; either version 2, or (at your option)
 # any later version.
 #
 # This program is distributed in the hope that it will be useful,

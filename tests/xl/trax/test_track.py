c898de30e9510b874a98c24f6f27dca365b4d2a0
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2009-12-26 19:28:26 -0500

    Got about 70% test coverage on track.py

--- tests/xl/trax/test_track.py
+++ tests/xl/trax/test_track.py
@@ -1,7 +1,14 @@
-import unittest
+# -*- coding: utf-8  -*-
+from __future__ import with_statement
+
 import os
+import shutil
+import tempfile
+import unittest
+import logging
 
 import mox
+import gio
 import gobject
 try:
     from nose.plugins.skip import SkipTest
@@ -9,13 +16,23 @@ except ImportError:
     SkipTest = None
 
 import xl.trax.track as track
+import xl.settings as settings
 
 
+LOG = logging.getLogger(__name__)
 TEST_TRACKS = [os.path.abspath(
     os.path.join(os.path.dirname(__file__), os.pardir, os.pardir,
     'data', 'music', 'delerium', 'chimera', '05 - Truly') + os.extsep + ext)
     for ext in ('aac', 'aiff', 'au', 'flac', 'mp3', 'mpc', 'ogg', 'spx',
                 'wav', 'wma', 'wv')]
+def get_file_with_ext(ext):
+    return [x for x in TEST_TRACKS if x.endswith(ext)][0]
+TEST_TRACKS_SIZE = {
+    get_file_with_ext('.mp3'): 4692,
+}
+
+def test_mp3_exists():
+    assert get_file_with_ext('.mp3')
 
 def test_all_tracks_exist():
     for track in TEST_TRACKS:
@@ -127,3 +144,157 @@ class TestTrack(unittest.TestCase):
             tr = track.Track(tr)
             self.assertTrue(tr.local_file_name())
             self.assertTrue(tr.exists())
+    
+    def test_local_type(self):
+        for tr in TEST_TRACKS:
+            tr = track.Track(tr)
+            self.assertEqual(tr.get_type(), 'file')
+
+    def test_write_tags(self):
+        # We test by creating a new file, changing the tags, writing tags
+        # and finally reopening a track with the name and seeing if it stuck
+        for tr_url in TEST_TRACKS:
+            # We run through this process with each filetype we have
+            suffix = os.extsep + tr_url.split(os.extsep)[-1]
+            # Stuff we can't actually write metadata to
+            if suffix in ('.aac', '.aiff', '.au', '.spx', '.wav'):
+                LOG.info("Skipping tag write test for " + suffix)
+                continue
+            # This fails. i don't feel like reading about it's failing for now
+            if suffix in ('.wma',):
+                LOG.critical("Skipping known failure :" + suffix)
+                continue
+            LOG.info("Testing writes for filetype: " + suffix)
+            with tempfile.NamedTemporaryFile(suffix=suffix) as temp_copy:
+                # Copy and write new file
+                shutil.copyfileobj(open(tr_url, 'r'), temp_copy)
+                tr = track.Track(temp_copy.name)
+                tr.set_tag_raw('artist', 'Delerium')
+                tr.write_tags()
+                del tr
+                tr = track.Track(temp_copy.name)
+                # Remove the artist tag and reread from file. This is done
+                # because of the whole flyweight thing
+                tr.set_tag_raw('artist', '')
+                tr.read_tags()
+                self.assertEqual(tr.get_tag_raw('artist'), [u'Delerium'])
+        raise SkipTest("Skipped known failure: .wma")
+
+    def test_write_tag_invalid_format(self):
+        tr = track.Track('/tmp/foo.foo')
+        self.assertEqual(tr.write_tags(), False)
+
+    def test_join_tag_empty(self):
+        """Tests get_tag_raw with join=True and an empty tag"""
+        tr = track.Track('foo')
+        self.assertEqual(tr.get_tag_raw('artist', join=True), None)
+
+    def test_join_tag_one(self):
+        """Tests get_tag_raw with join=True and one element in tag"""
+        tr = track.Track('foo')
+        tr.set_tag_raw('artist', 'foo')
+        self.assertEqual(tr.get_tag_raw('artist', join=True), u'foo')
+
+    def test_join_tag_two(self):
+        """Tests get_tag_raw with join=True and one element in tag"""
+        tr = track.Track('foo')
+        tr.set_tag_raw('artist', [u'foo', u'bar'])
+        self.assertEqual(tr.get_tag_raw('artist', join=True), u'foo / bar')
+
+    def test_is_local_local(self):
+        """Tests a local filename -> True"""
+        tr = track.Track('foo')
+        self.assertEqual(tr.is_local(), True)
+
+    def test_is_local_remote(self):
+        """Tests a remote filename -> False"""
+        tr = track.Track('http://foo')
+        self.assertEqual(tr.is_local(), False)
+
+    def test_local_filesize(self):
+        for tr_name in TEST_TRACKS_SIZE:
+            tr = track.Track(tr_name)
+            self.assertEqual(tr.get_size(), TEST_TRACKS_SIZE[tr_name])
+
+    def test_str(self):
+        tr = track.Track('foo')
+        self.assertEqual(str(tr), "'Unknown' from 'Unknown' by 'Unknown'")
+
+    def test_pickles(self):
+        tr = track.Track('/foo')
+        tr.set_tag_raw('artist', 'bar')
+        self.assertEqual(tr._pickles(), {
+            '__loc': 'file:///foo',
+            'artist': [u'bar']
+            })
+    
+    def test_list_tags(self):
+        tr = track.Track('/foo')
+        tr.set_tag_raw('artist', 'foo')
+        tr.set_tag_raw('album', 'bar')
+        self.assertEqual(tr.list_tags(), ['album', '__loc', 'artist'])
+
+    def test_rating_empty(self):
+        """Test get_rating when no rating has been set"""
+        tr = track.Track('/foo')
+        self.assertEqual(tr.get_rating(), 0)
+
+    def test_set_rating(self):
+        tr = track.Track('/foo')
+        tr.set_rating(2)
+        self.assertEqual(tr.get_rating(), 2)
+
+    def test_set_rating_invalid(self):
+        raise SkipTest("We currently do not properly do things on incorrect"
+                "values passed to set_rating")
+        tr = track.Track('/bar')
+        self.assertRaises(ValueError, tr.set_rating, 'foo')
+
+    def test_split_numerical_none(self):
+        self.assertEqual(track.Track.split_numerical(None), (0, 0))
+
+    def test_split_numerical_str(self):
+        fn = track.Track.split_numerical
+        self.assertEqual(fn('12/15'), (12, 15))
+        self.assertEqual(fn('foo/15'), (0, 15))
+        self.assertEqual(fn('12/foo'), (12, 0))
+        self.assertEqual(fn('12/15/2009'), (12, 15))
+
+    def test_split_numerical_list(self):
+        fn = track.Track.split_numerical
+        self.assertEqual(fn(['12/15']), (12, 15))
+        self.assertEqual(fn(['foo/15']), (0, 15))
+        self.assertEqual(fn(['12/foo']), (12, 0))
+        self.assertEqual(fn(['12/15/2009']), (12, 15))
+
+    def test_strip_leading(self):
+        #Strips whitespace
+        value = " `~!@#$%^&*()_+-={}|[]\\\";'<>?,./"
+        retvalue = "`~!@#$%^&*()_+-={}|[]\\\";'<>?,./"
+        self.assertEqual(track.Track.strip_leading(value), retvalue)
+        self.assertEqual(track.Track.strip_leading(value + "foo"), "foo")
+
+    def test_cutter(self):
+        value = 'the a foo'
+        track.Track._Track__the_cuts = ['the', 'a']
+        self.assertEqual(track.Track.the_cutter(value), 'a foo')
+
+    def test_expand_doubles(self):
+        value = u'ßæĳŋœƕǆǉǌǳҥҵ'
+        self.assertEqual(track.Track.expand_doubles(value),
+                u'ssaeijngoehvdzljnjdzngts')
+
+    def test_lower(self):
+        value = u'FooBar'
+        self.assertEqual(track.Track.lower(value), 'foobar FooBar')
+
+    def test_cuts_cb(self):
+        value = []
+        settings.set_option('collection/strip_list', value)
+        track.Track._the_cuts_cb(None, None, 'collection/strip_list')
+        self.assertEqual(track.Track._Track__the_cuts, value)
+
+        value = ['the', 'foo']
+        settings.set_option('collection/strip_list', value)
+        track.Track._the_cuts_cb(None, None, 'collection/strip_list')
+        self.assertEqual(track.Track._Track__the_cuts, value)

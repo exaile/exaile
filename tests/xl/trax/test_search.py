e515f83604e3133216fc27946e5e0d3b4fe75d3b
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2010-01-08 23:49:56 -0500

    Started testing TrackMatcher

--- tests/xl/trax/test_search.py
+++ tests/xl/trax/test_search.py
@@ -4,18 +4,22 @@ import unittest
 from xl.trax import search
 from xl.trax import track
 
+
 def test_search_result_track_get_track():
     val = 'foo'
     search_result_track = search.SearchResultTrack(val)
     assert search_result_track.track == val, search_result_track.track
 
+
 def get_search_result_track():
     tr = track.Track('file:///foo')
     return search.SearchResultTrack(tr)
 
+
 def clear_all_tracks():
     for key in track.Track._Track__tracksdict.keys():
         del track.Track._Track__tracksdict[key]
+
 
 class TestMatcher(unittest.TestCase):
 
@@ -57,6 +61,7 @@ class TestMatcher(unittest.TestCase):
         matcher = search._Matcher('album', None, lambda x: x)
         self.assertRaises(NotImplementedError, matcher._matches, 'foo')
 
+
 class TestExactMatcher(unittest.TestCase):
 
     def setUp(self):
@@ -74,6 +79,7 @@ class TestExactMatcher(unittest.TestCase):
         matcher = search._ExactMatcher('album', 'Foo', lambda x: x)
         self.str.track.set_tag_raw('album', 'FoO')
         self.assertFalse(matcher.match(self.str))
+
 
 class TestInMatcher(unittest.TestCase):
 
@@ -103,6 +109,7 @@ class TestInMatcher(unittest.TestCase):
         self.str.track.set_tag_raw('album', 'Fooheloworld')
         self.assertFalse(matcher.match(self.str))
 
+
 class TestMetaMatcherClasses(unittest.TestCase):
 
     class _Matcher(object):
@@ -116,6 +123,7 @@ class TestMetaMatcherClasses(unittest.TestCase):
         def match(self, val):
             return self.val
 
+
 class TestNotMetaMatcher(TestMetaMatcherClasses):
 
     def test_true(self):
@@ -127,6 +135,7 @@ class TestNotMetaMatcher(TestMetaMatcherClasses):
         matcher = self._Matcher(False)
         matcher = search._NotMetaMatcher(matcher)
         self.assertTrue(matcher.match('foo'))
+
 
 class TestOrMetaMatcher(TestMetaMatcherClasses):
 
@@ -154,7 +163,8 @@ class TestOrMetaMatcher(TestMetaMatcherClasses):
         matcher = search._OrMetaMatcher(matcher_1, matcher_2)
         self.assertFalse(matcher.match('foo'))
 
-class TestAnyMetaMatcher(TestMetaMatcherClasses):
+
+class TestMultiMetaMatcher(TestMetaMatcherClasses):
 
     def test_true(self):
         matcher = [self._Matcher(True)] * 10
@@ -166,3 +176,145 @@ class TestAnyMetaMatcher(TestMetaMatcherClasses):
         matcher = search._MultiMetaMatcher(matcher)
         self.assertFalse(matcher.match('foo'))
 
+
+class TestManyMultiMetaMatcher(TestMetaMatcherClasses):
+
+    def test_true(self):
+        matcher = [self._Matcher(True)] * 10 + [self._Matcher(False)]
+        for match in matcher:
+            match.tag = 'artist'
+        matcher = search._ManyMultiMetaMatcher(matcher)
+        self.assertTrue(matcher.match('foo'))
+
+    def test_false(self):
+        matcher = [self._Matcher(False)] * 10
+        for match in matcher:
+            match.tag = 'artist'
+        matcher = search._ManyMultiMetaMatcher(matcher)
+        self.assertFalse(matcher.match('foo'))
+
+
+class TestTracksMatcher(unittest.TestCase):
+
+    def setUp(self):
+        self.str = get_search_result_track()
+
+    def test_in_matcher(self):
+        matcher = search.TracksMatcher("artist=foo")
+        self.assertEqual(len(matcher.matchers), 1)
+        match = matcher.matchers[0]
+        self.match_is_type(match, search._InMatcher)
+        self.assertEqual(match.tag, 'artist')
+        self.assertEqual(match.content, 'foo')
+
+    def test_exact_matcher(self):
+        matcher = search.TracksMatcher("artist==foo")
+        self.assertEqual(len(matcher.matchers), 1)
+        match = matcher.matchers[0]
+        self.match_is_type(match, search._ExactMatcher)
+        self.assertEqual(match.tag, 'artist')
+        self.assertEqual(match.content, 'foo')
+
+    def match_is_type(self, match, expected):
+        self.assertTrue(isinstance(match, expected), match)
+
+    def test_not_matcher(self):
+        matcher = search.TracksMatcher("! foo", keyword_tags=['artist'])
+        match = matcher
+        # NotMetaMatcher
+        self.assertEqual(len(match.matchers), 1)
+        match = matcher.matchers[0]
+        self.match_is_type(match, search._NotMetaMatcher)
+        # MultiMetaMatcher
+        match = match.matcher
+        self.match_is_type(match, search._MultiMetaMatcher)
+        # ManyMultiMetaMatcher
+        self.assertEqual(len(match.matchers), 1)
+        match = match.matchers[0]
+        self.match_is_type(match, search._ManyMultiMetaMatcher)
+        # InMatcher
+        self.assertEqual(len(match.matchers), 1)
+        match = match.matchers[0]
+        self.match_is_type(match, search._InMatcher)
+        self.assertEqual(match.tag, 'artist')
+        self.assertEqual(match.content, 'foo')
+
+    def test_or_matcher(self):
+        matcher = search.TracksMatcher("foo | bar", keyword_tags=['artist'])
+        match = matcher
+        # OrMetaMatcher
+        self.assertEqual(len(match.matchers), 1)
+        match = matcher.matchers[0]
+        self.match_is_type(match, search._OrMetaMatcher)
+        # MultiMetaMatcher
+        self.assertTrue(match.left)
+        self.assertTrue(match.right)
+        self.match_is_type(match.left, search._MultiMetaMatcher)
+        self.match_is_type(match.right, search._MultiMetaMatcher)
+        # ManyMultiMetaMatcher
+        self.assertEqual(len(match.left.matchers), 1)
+        self.assertEqual(len(match.right.matchers), 1)
+        match_left = match.left.matchers[0]
+        match_right = match.right.matchers[0]
+        self.match_is_type(match_left, search._ManyMultiMetaMatcher)
+        self.match_is_type(match_right, search._ManyMultiMetaMatcher)
+        # InMatcher
+        self.assertEqual(len(match_left.matchers), 1)
+        self.assertEqual(len(match_right.matchers), 1)
+        match_left = match_left.matchers[0]
+        match_right = match_right.matchers[0]
+        self.match_is_type(match_left, search._InMatcher)
+        self.match_is_type(match_right, search._InMatcher)
+        self.assertEqual(match_left.tag, 'artist')
+        self.assertEqual(match_right.tag, 'artist')
+        if match_left.content == 'foo':
+            if match_right.content != 'bar':
+                self.assertFalse("We lost a search term on an or")
+        elif match_left.content == 'bar':
+            if match_right.content != 'foo':
+                self.assertFalse("We lost a search term on an or")
+        else:
+            self.assertFalse("We lost both parts of an or")
+
+    def test_paren_matcher(self):
+        matcher = search.TracksMatcher("( foo | bar )",
+                keyword_tags=['artist'])
+        match = matcher
+        # MultiMetaMatcher
+        self.assertEqual(len(match.matchers), 1)
+        match = matcher.matchers[0]
+        self.match_is_type(match, search._MultiMetaMatcher)
+
+        self.assertEqual(len(match.matchers), 1)
+        match = match.matchers[0]
+        self.match_is_type(match, search._OrMetaMatcher)
+
+        # This is the same code as the OrMetaMatcher
+        self.assertTrue(match.left)
+        self.assertTrue(match.right)
+        self.match_is_type(match.left, search._MultiMetaMatcher)
+        self.match_is_type(match.right, search._MultiMetaMatcher)
+        # ManyMultiMetaMatcher
+        self.assertEqual(len(match.left.matchers), 1)
+        self.assertEqual(len(match.right.matchers), 1)
+        match_left = match.left.matchers[0]
+        match_right = match.right.matchers[0]
+        self.match_is_type(match_left, search._ManyMultiMetaMatcher)
+        self.match_is_type(match_right, search._ManyMultiMetaMatcher)
+        # InMatcher
+        self.assertEqual(len(match_left.matchers), 1)
+        self.assertEqual(len(match_right.matchers), 1)
+        match_left = match_left.matchers[0]
+        match_right = match_right.matchers[0]
+        self.match_is_type(match_left, search._InMatcher)
+        self.match_is_type(match_right, search._InMatcher)
+        self.assertEqual(match_left.tag, 'artist')
+        self.assertEqual(match_right.tag, 'artist')
+        if match_left.content == 'foo':
+            if match_right.content != 'bar':
+                self.assertFalse("We lost a search term on an or")
+        elif match_left.content == 'bar':
+            if match_right.content != 'foo':
+                self.assertFalse("We lost a search term on an or")
+        else:
+            self.assertFalse("We lost both parts of an or")

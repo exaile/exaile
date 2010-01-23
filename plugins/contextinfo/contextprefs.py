382a1d95efcfe742868d0202e298269dd4c2c27c
Aren Olson <reacocard@gmail.com>
2010-01-23 13:15:41 -0600

    forgot to add these. oops.

---
+++ plugins/contextinfo/contextprefs.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2010 Guillaume Lecomte
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 1, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+import os
+from xlgui.prefs import widgets
+from xl import xdg
+from xl.nls import gettext as _
+
+name = _('Contextinfo')
+basedir = os.path.dirname(os.path.realpath(__file__))
+ui = os.path.join(basedir, "context_pane.ui")
+
+class UserPreference(widgets.PrefsItem):
+    name = 'plugin/lastfm/user'
+
+class PassPreference(widgets.HashedPrefsItem):
+    name = 'plugin/lastfm/password'
+    type = 'md5'

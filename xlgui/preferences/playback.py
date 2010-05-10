3ce4a9cc8442039f69f5aef8219ad9384e7abe23
Mathias Brodala <info@noctus.net>
2010-05-10 19:10:09 +0200

    * Prompt for restart after changing playback engine

--- xlgui/preferences/playback.py
+++ xlgui/preferences/playback.py
@@ -24,8 +24,11 @@
 # do so. If you do not wish to do so, delete this exception statement
 # from your version.
 
+import gobject
+import gtk
+
 from xlgui.preferences import widgets
-from xl import xdg
+from xl import main, xdg
 from xl.nls import gettext as _
 from xlgui import commondialogs
 
@@ -38,6 +41,40 @@ class EnginePreference(widgets.ComboPreference):
 
     def __init__(self, preferences, widget):
         widgets.ComboPreference.__init__(self, preferences, widget)
+
+        self.dialog = gtk.MessageDialog(
+            self.preferences.window,
+            gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+            gtk.MESSAGE_QUESTION)
+        self.dialog.set_title(_('Restart required'))
+        self.dialog.set_markup(_('This change requires a restart '
+                            'of Exaile to take effect.\n'
+                            '<b>Do you want to restart now?</b>'))
+        self.dialog.add_button(gtk.STOCK_CLOSE, gtk.RESPONSE_CLOSE)
+        button = self.dialog.add_button(_('Restart'), gtk.RESPONSE_ACCEPT)
+        button.set_image(gtk.image_new_from_stock(
+            gtk.STOCK_REFRESH, gtk.ICON_SIZE_BUTTON))
+
+    def apply(self, value=None):
+        """
+            Applies this setting
+        """
+        oldvalue = self.preferences.settings.get_option(
+            self.name, self.default)
+
+        if value is None:
+            value = self._get_value()
+
+        if value != oldvalue:
+            self.preferences.settings.set_option(self.name, value)
+
+            response = self.dialog.run()
+            self.dialog.hide()
+
+            if response == gtk.RESPONSE_ACCEPT:
+                gobject.idle_add(main.exaile().quit, True)
+
+        return True
 
 class AudioSinkPreference(widgets.ComboPreference):
     default = "auto"

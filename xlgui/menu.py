38dbebb67bab8679b7d254377a2382d0fa2551e4
Mathias Brodala <info@noctus.net>
2010-07-21 09:12:51 +0200

    * Turn "Open" menu item into stock again * Rename "Open URI" back to "Open URL" and re-add icon

--- xlgui/menu.py
+++ xlgui/menu.py
@@ -46,30 +46,43 @@ _sep = menu.simple_separator
 def __create_file_menu():
     items = []
     accelerators = []
+
     def new_playlist_cb(*args):
         get_main().playlist_notebook.create_new_playlist()
-    items.append(_smi('new-playlist', [], _("_New Playlist"), 'tab-new', new_playlist_cb, accelerator='<Control>t'))
+    items.append(_smi('new-playlist', [], _("_New Playlist"), 'tab-new',
+        new_playlist_cb, accelerator='<Control>t'))
     accelerators.append(Accelerator('<Control>t', new_playlist_cb))
     items.append(_sep('new-sep', [items[-1].name]))
+
     def open_cb(*args):
         dialog = dialogs.MediaOpenDialog(get_main().window)
-        dialog.connect('uris-selected', lambda d, uris: get_main().controller.open_uris(uris))
+        dialog.connect('uris-selected', lambda d, uris:
+            get_main().controller.open_uris(uris))
         dialog.show()
-    items.append(_smi('open', [items[-1].name], _("_Open"), 'gtk-open', open_cb, accelerator='<Control>o'))
+    items.append(_smi('open', [items[-1].name], icon_name=gtk.STOCK_OPEN,
+        callback=open_cb, accelerator='<Control>o'))
     accelerators.append(Accelerator('<Control>o', open_cb))
+
     def open_uri_cb(*args):
         dialog = dialogs.URIOpenDialog(get_main().window)
-        dialog.connect('uri-selected', lambda d, uri: get_main().controller.open_uri(uri))
+        dialog.connect('uri-selected', lambda d, uri:
+            get_main().controller.open_uri(uri))
         dialog.show()
-    items.append(_smi('open-uri', [items[-1].name], _("Open _URI"), None, open_uri_cb, accelerator='<Control><Shift>o'))
+    items.append(_smi('open-uri', [items[-1].name], _("Open _URL"),
+        'applications-internet', open_uri_cb, accelerator='<Control><Shift>o'))
     accelerators.append(Accelerator('<Control><Shift>o', open_uri_cb))
+
     def open_dirs_cb(*args):
         dialog = dialogs.DirectoryOpenDialog(get_main().window)
         dialog.props.create_folders = False
-        dialog.connect('uris-selected', lambda d, uris: get_main().controller.open_uris(uris))
+        dialog.connect('uris-selected', lambda d, uris:
+            get_main().controller.open_uris(uris))
         dialog.show()
-    items.append(_smi('open-dirs', [items[-1].name], _("Open Directories"), None, open_dirs_cb))
+    items.append(_smi('open-dirs', [items[-1].name], _("Open Directories"),
+        None, open_dirs_cb))
+
     items.append(_sep('open-sep', [items[-1].name]))
+
     def export_playlist_cb(*args):
         main = get_main()
         page = get_selected_playlist()
@@ -87,8 +100,10 @@ def __create_file_menu():
         dialog = dialogs.PlaylistExportDialog(page.playlist, main.window)
         dialog.connect('message', on_message)
         dialog.show()
-    items.append(_smi('export-playlist', [items[-1].name], _("_Export Current Playlist"), 'gtk-save-as', export_playlist_cb))
+    items.append(_smi('export-playlist', [items[-1].name],
+        _("_Export Current Playlist"), 'gtk-save-as', export_playlist_cb))
     items.append(_sep('export-sep', [items[-1].name]))
+
     if get_main().controller.exaile.options.Debug:
         def restart_cb(*args):
             from xl import main
@@ -96,12 +111,14 @@ def __create_file_menu():
         items.append(_smi('restart-application', [items[-1].name], _("Restart"),
             callback=restart_cb, accelerator='<Control>r'))
         accelerators.append(Accelerator('<Control>r', restart_cb))
+
     def quit_cb(*args):
         from xl import main
         main.exaile().quit()
     items.append(menu.simple_menu_item('quit-application', [items[-1].name],
         icon_name=gtk.STOCK_QUIT, callback=quit_cb))
     accelerators.append(Accelerator('<Control>q', quit_cb))
+
     for item in items:
         providers.register('menubar-file-menu', item)
     for accelerator in accelerators:

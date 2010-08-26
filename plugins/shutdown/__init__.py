d8aa1f300aabfd8f05fc54647eadc7523bac5682
Mathias Brodala <info@noctus.net>
2010-08-26 02:10:05 +0200

    * Shutdown plugin: Prefer ConsoleKit * Ditto: Use MessageBar including cancellable countdown before shutdown (Fixes: #624346)

--- plugins/shutdown/__init__.py
+++ plugins/shutdown/__init__.py
@@ -13,16 +13,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 import dbus
+import glib
 import gtk
-try:
-    import pynotify
-except ImportError:
-    pynotify = None
-    from xlgui.widgets import dialogs
+
 from xl import event
 from xl.nls import gettext as _
 from xlgui import icons
+from xlgui.widgets import dialogs
 
 SHUTDOWN = None
 
@@ -34,6 +33,13 @@ class Shutdown():
         self.menu_item.set_tooltip_text(_('Shutdown computer at the end of playback'))
         exaile.gui.builder.get_object('tools_menu').append(self.menu_item)
         self.menu_item.show()
+        self.countdown = None
+        self.counter = 10
+
+        self.message = dialogs.MessageBar(
+            parent=exaile.gui.builder.get_object('player_box'),
+            buttons=gtk.BUTTONS_CLOSE)
+        self.message.connect('response', self.on_response)
 
     def on_toggled(self, menuitem):
         """
@@ -42,45 +48,83 @@ class Shutdown():
         if menuitem.get_active():
             event.add_callback(self.on_playback_player_end, 'playback_player_end')
 
-            if pynotify:
-                pynotify.init('Exaile')
-                notification = pynotify.Notification(_('<b>Shutdown scheduled</b>'),
-                    _('Computer will be shutdown at the end of playback.'))
-                notification.set_icon_from_pixbuf(icons.MANAGER.pixbuf_from_stock(
-                    gtk.STOCK_QUIT, gtk.ICON_SIZE_DIALOG))
-                notification.show()
-            else:
-                dialogs.info(None, _('<b>Shutdown scheduled</b>\n\n'
-                    'Computer will be shutdown at the end of playback.'))
+            self.message.show_info(_('Shutdown scheduled'),
+                _('Computer will be shutdown at the end of playback.'))
         else:
             event.remove_callback(self.on_playback_player_end, 'playback_player_end')
+
+            # Stop possible countdown
+            if self.countdown is not None:
+                glib.source_remove(self.countdown)
+                self.countdown = None
+
+            # Prepare for a new run
+            self.counter = 10
+
+            # Reset message button layout
+            self.message.hide()
+            self.message.clear_buttons()
+            self.message.add_button(gtk.STOCK_CLOSE, gtk.RESPONSE_CLOSE)
 
     def on_playback_player_end(self, event, player, track):
         """
             Tries to shutdown the computer
         """
+        self.message.set_message_type(gtk.MESSAGE_INFO)
+        self.message.set_markup(_('Imminent Shutdown'))
+        self.message.clear_buttons()
+        self.message.add_button(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
+
+        if self.countdown is not None:
+            glib.source_remove(self.countdown)
+
+        self.counter = 10
+        self.countdown = glib.timeout_add_seconds(1, self.on_timeout)
+
+    def on_response(self, widget, response):
+        """
+            Cancels shutdown if requested
+        """
+        if response == gtk.RESPONSE_CANCEL:
+            self.menu_item.set_active(False)
+
+    def on_timeout(self):
+        """
+            Tries to shutdown the computer
+        """
+        if self.counter > 0:
+            self.message.set_secondary_text(
+                _('The computer will be shut down in %d seconds.') % self.counter)
+            self.message.show()
+
+            self.counter -= 1;
+
+            return True
+
+        self.menu_item.set_active(False)
+
         bus = dbus.SystemBus()
 
         try:
-            remote_object = bus.get_object('org.freedesktop.Hal',
-                '/org/freedesktop/Hal/devices/computer')
-            remote_object.Shutdown(dbus_interface='org.freedesktop.Hal.Device.SystemPowerManagement')
+            proxy = bus.get_object('org.freedesktop.ConsoleKit',
+                '/org/freedesktop/ConsoleKit/Manager')
+            proxy.Stop(dbus_interface='org.freedesktop.ConsoleKit.Manager')
         except dbus.exceptions.DBusException:
-            if pynotify:
-                pynotify.init('Exaile')
-                notification = pynotify.Notification(_('<b>Shutdown failed</b>'),
+            try:
+                proxy = bus.get_object('org.freedesktop.Hal',
+                    '/org/freedesktop/Hal/devices/computer')
+                proxy.Shutdown(dbus_interface='org.freedesktop.Hal.Device.SystemPowerManagement')
+            except dbus.exceptions.DBusException:
+                self.message.show_warning(_('Shutdown failed'),
                     _('Computer could not be shutdown using D-Bus.'))
-                notification.set_icon_from_pixbuf(icons.MANAGER.pixbuf_from_stock(
-                    gtk.STOCK_DIALOG_ERROR, gtk.ICON_SIZE_DIALOG))
-                notification.show()
-            else:
-                dialogs.error(None, _('<b>Shutdown failed</b>\n\n'
-                    'Computer could not be shutdown using D-Bus.'))
 
     def destroy(self):
         """
             Cleans up
         """
+        if self.countdown is not None:
+            glib.source_remove(self.countdown)
+
         event.remove_callback(self.on_playback_player_end, 'playback_player_end')
         self.menu_item.hide()
         self.menu_item.destroy()

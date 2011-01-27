474939bd16793d45199adf1ed27d8fc9821f2419
Johannes Sasongko <sasongko@gmail.com>
2011-01-27 12:02:01 +1000

    Categorize plugins based on whether they work and how useful they are.
    - good = works
      - main = useful (make install)
      - extra = not very useful (make extra_install)
    - bad = doesn't work
    
    Current list:
    - bad: shoutcast
    - good:
      - extra: helloworld
      - main: (the rest)

--- plugins/list.py
+++ plugins/list.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python
 
-import os
-files = []
-for file in os.listdir('.'):
-    if os.path.isdir(file):
-        files.append(file)
+from __future__ import print_function
 
-print (' '.join(files))
+import os, sys
+
+# - good = works
+#   - main = useful
+#   - extra = not very useful
+# - bad = doesn't work
+
+EXTRA = ['helloworld']
+BAD = ['shoutcast']
+
+def scan():
+    all = set(f for f in os.listdir('.') if os.path.isdir(f))
+    bad = set(BAD)
+    good = all - bad
+    extra = set(EXTRA)
+    main = good - extra
+    return locals()
+
+plugins = scan()
+
+def parse(argv):
+    if len(argv) == 1:
+        return plugins['all']
+    return plugins[argv[1]]
+
+if __name__ == '__main__':
+    names = list(parse(sys.argv))
+    names.sort()
+    print(' '.join(names))

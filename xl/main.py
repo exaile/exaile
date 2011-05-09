6f048524bf5f25ccab5687e5ba0f00354e1883d9
Mathias Brodala <info@noctus.net>
2011-05-09 19:41:47 +0200

    * Remove ASCII art from version output

--- xl/main.py
+++ xl/main.py
@@ -531,11 +531,7 @@ class Exaile(object):
 
     def version(self):
         from xl.version import __version__
-        print r"""   ____          _ __    __
-  / __/_ _____ _(_) /__ / /
- / _/ \ \ / _ `/ / / -_)_/
-/___//_\_\\_,_/_/_/\__(_)   v%s
-"""%__version__
+        print __version__
         exit()
 
     def _add_default_playlists(self):

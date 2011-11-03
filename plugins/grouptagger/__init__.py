019828c29b0c8af1811b0fc9f4dc49e784e96bc2
Dustin Spicuzza <dustin@virtualroadside.com>
2011-11-03 11:22:40 -0400

    Added first version of the grouptagger plugin. Still needs some extra features, but should be 100% functional at this point

---
+++ plugins/grouptagger/__init__.py
@@ -0,0 +1,275 @@
+# Copyright (C) 2011 Dustin Spicuzza
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+#
+# The developers of the Exaile media player hereby grant permission
+# for non-GPL compatible GStreamer and Exaile plugins to be used and
+# distributed together with GStreamer and Exaile. This permission is
+# above and beyond the permissions granted by the GPL license by which
+# Exaile is covered. If you modify this code, you may extend this
+# exception to your version of the code, but you are not obligated to
+# do so. If you do not wish to do so, delete this exception statement
+# from your version.
+
+
+import os
+import time
+import gtk
+import gobject
+ 
+from xl import (
+    event, 
+    providers,
+    player,
+    settings
+)
+
+from xl.nls import gettext as _
+from xlgui import guiutil
+from xlgui.widgets import menu, dialogs
+
+import gt_widgets
+
+plugin = None
+
+
+def enable(exaile):
+    '''Called on plugin enable'''
+    if exaile.loading:
+        event.add_callback(_enable, 'gui_loaded')
+    else:
+        _enable(None, exaile, None)
+        
+def _enable(eventname, exaile, nothing):
+
+    global plugin
+    plugin = GroupTaggerPlugin(exaile)
+    
+def disable(exaile):
+    '''Called on plugin disable'''
+    
+    global plugin
+    if plugin is not None:
+        plugin.disable_plugin(exaile)
+        plugin = None
+   
+    
+class GroupTaggerPlugin(object):
+    '''Implements logic for plugin'''
+
+    def __init__(self, exaile):
+    
+        self.track = None
+    
+        self.panel = gt_widgets.GroupTaggerPanel()
+        self.panel.show_all()
+        
+        self.panel.tagger.view.connect( 'changed', self.on_group_changed )
+        
+        exaile.gui.add_panel( self.panel, _('GroupTagger') )
+        
+        # ok, register for some events
+        event.add_callback( self.on_playback_track_start, 'playback_track_start' )
+        event.add_callback( self.on_playlist_cursor_changed, 'playlist_cursor_changed' )
+        
+        #self.playlist_context_menuitem = menu.simple_menu_item(
+        #    'grouptagger_ctx_menu', ['rating'], _('GroupTagger'), 
+        #    callback=self.on_playlist_context_menu )
+        #providers.register( 'playlist-context-menu', self.playlist_context_menuitem )
+        
+        # trigger start event if exaile is currently playing something
+        if player.PLAYER.is_playing():
+            self.set_display_track( None, player.PLAYER, player.PLAYER.current )
+        else:
+            self.panel.tagger.set_groups( [(False, group) for group in get_default_groups()] )
+    
+    def disable_plugin(self, exaile):
+        '''Called when the plugin is disabled'''
+        
+        #if self.playlist_context_menuitem:
+        #    providers.unregister( 'playlist-context-menu', self.playlist_context_menuitem )
+        #    self.playlist_context_menuitem = None
+        
+        # de-register the exaile events
+        event.remove_callback( self.playback_track_start, 'playback_track_start' )
+        
+        exaile.gui.remove_panel( self.panel )
+        
+        
+    #
+    # Exaile events
+    #
+        
+    @guiutil.idle_add()
+    def on_playback_track_start(self, type, player, track):
+        '''Called when a new track starts'''
+        self.set_display_track( track )
+        
+        
+    #def on_playlist_context_menu( self, menu, display_name, playlist_view, context ):
+    #    '''Called when our context menu item is selected'''
+    #    tracks = context['selected-tracks']
+    #    if len(tracks) == 1:
+    #        self.set_display_track( tracks[0] )
+            
+    def on_playlist_cursor_changed( self, type, playlist_view, context ):
+        '''Called when an item in a playlist is selected'''
+        tracks = context['selected-tracks']
+        if len(tracks) == 1:
+            self.set_display_track( tracks[0] )
+        
+
+    def set_display_track(self, track):
+        '''Updates the display with the tags/info for a particular track'''
+        self.track = track
+        
+        # get the groups as a set
+        track_groups = get_track_groups( track )
+        
+        # get the default groups
+        default_groups = get_default_groups()
+        
+        # setup something appropriate to display
+        groups = [(True, group) for group in track_groups]
+        
+        # add any defaults not present
+        groups.extend( [ (False, group) for group in default_groups.difference( track_groups )] )
+        
+        # set them
+        self.panel.tagger.view.show_click_column()
+        self.panel.tagger.set_groups( groups )
+        self.panel.tagger.set_track_info( track )
+        
+    #
+    # Widget events
+    #
+        
+    def on_group_changed(self, view, action, value):
+        '''Called when a group is added/deleted/etc on the widget'''
+        
+        groups = view.get_model().get_active_groups()
+        
+        if self.track is not None:
+            write_succeeded = set_track_groups( self.track, groups )
+        
+        #
+        # use the action to determine how to update the global
+        # defaults: If the user explcitly added/deleted objects,
+        # then we add/remove those to/from the globals if required
+        #
+        
+        if action == gt_widgets.CHANGE_ADDED or action == gt_widgets.CHANGE_DELETED:
+        
+            all_groups = get_default_groups()
+            sz = len( all_groups )
+        
+            if action == gt_widgets.CHANGE_ADDED:
+                all_groups.add( value )
+            else:
+                all_groups.discard( value )
+        
+            # if it changed, set it
+            if sz != len( all_groups ):
+                set_default_groups( all_groups )
+        
+        if self.track is not None and not write_succeeded:
+            self.set_display_track( self.track )
+  
+#
+# Grouping field utility functions
+#
+  
+
+def get_track_groups(track):
+    '''
+        Returns a set() of groups present in this track
+    '''
+    grouping = track.get_tag_raw('grouping', True)
+    
+    if grouping is not None:
+        return set([ group.replace('_', ' ') for group in grouping.split()])
+        
+    return set()
+
+
+def set_track_groups(track, groups):
+    '''
+        Given an array of groups, sets them on a track
+        
+        Returns true if successful, false if there was an error
+    '''
+    
+    grouping = ' '.join( sorted( [ '_'.join( group.split() ) for group in groups ] ) )
+    track.set_tag_raw( 'grouping', grouping )
+    
+    if not track.write_tags():
+        dialogs.error( None, "Error writing tags to %s" % gobject.markup_escape_text(track.get_loc_for_io()) )
+        return False
+        
+    return True
+
+
+def get_default_groups():
+    '''
+        Returns a set() of groups stored in the settings that the user can
+        easily select without having to retype it
+    '''
+    
+    default_groups = settings.get_option( 'plugin/grouptagger/default_groups', set() )
+    return set(default_groups)
+
+    
+    
+def set_default_groups(groups):
+    '''
+        Stores the default groups as a list
+    '''
+    settings.set_option( 'plugin/grouptagger/default_groups', list(groups) )
+    
+
+    
+#
+# TODO: Fix these
+#
+ 
+        
+def generate_playlist( group, playlist_name ):
+    # creates a smart playlist based on a particular tag
+    pass
+    
+def generate_playlists( ):
+
+    # what this function does is create playlists based on all the tags present in the library
+    tracks = get_all_tracks()
+    
+    groupset = set()
+    
+    # TODO: I wonder if there's a way we can cache this data, or
+    # get exaile to do it for us.. 
+    for track in tracks:
+        groups = get_track_groups( track )
+        groupset |= groups
+        
+    prefix = settings.get_option( 'plugins/grouptagger/auto-playlist-prefix', 'Auto-' )
+    for group in groupset:
+        playlist_name = prefix + group
+        if playlist_name not in playlists:
+            generate_playlist( group, playlist_name )
+        
+
+   
+
+    
---
+++ plugins/grouptagger/gt_widgets.py
@@ -0,0 +1,293 @@
+# Copyright (C) 2011 Dustin Spicuzza
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+#
+# The developers of the Exaile media player hereby grant permission
+# for non-GPL compatible GStreamer and Exaile plugins to be used and
+# distributed together with GStreamer and Exaile. This permission is
+# above and beyond the permissions granted by the GPL license by which
+# Exaile is covered. If you modify this code, you may extend this
+# exception to your version of the code, but you are not obligated to
+# do so. If you do not wish to do so, delete this exception statement
+# from your version.
+
+
+import gtk
+import gobject
+import pango
+import glib
+
+from xl.nls import gettext as _
+
+from xlgui.widgets import dialogs
+
+#
+# GroupTaggerView signal 'changed' enum
+#
+
+# group was added to tagger
+CHANGE_ADDED = object()
+# group was deleted from tagger
+CHANGE_DELETED = object()
+# group was edited/toggled in tagger, pass none
+CHANGE_OTHER = object()
+
+
+class GroupTaggerView(gtk.TreeView):
+    '''Treeview widget to display tag lists'''
+
+    __gsignals__ = {
+        # param1: see change enum above for the type of change, param2: value that changed
+        'changed': (gobject.SIGNAL_ACTION, gobject.TYPE_NONE, (gobject.TYPE_PYOBJECT, gobject.TYPE_STRING) ),
+    }
+    
+    def __init__(self, model=None):
+    
+        gtk.TreeView.__init__(self,model)
+        
+        self.set_enable_search( False )
+        self.get_selection().set_mode( gtk.SELECTION_MULTIPLE )
+        
+        # Setup the first column
+        cell = gtk.CellRendererToggle()
+        cell.set_property( 'mode', gtk.CELL_RENDERER_MODE_ACTIVATABLE )
+        cell.set_activatable( True )
+        cell.connect( 'toggled', self.on_toggle, model )
+        
+        self.click_column = gtk.TreeViewColumn( None, cell, active=0 )
+        
+        # Setup the second column
+        cell = gtk.CellRendererText()
+        cell.set_property( 'editable', True )
+        cell.connect( 'edited', self.on_edit, model )
+        
+        self.append_column( gtk.TreeViewColumn( _('Group'), cell, text=1 ) )
+        
+        self.connect( 'popup-menu', self.on_popup_menu )
+        self.connect( 'button-press-event', self.on_mouse_press )
+        
+        self.menu = gtk.Menu()
+        
+        item = gtk.MenuItem( _('Add new group') )
+        item.connect( 'activate', self.on_menu_add_group )
+        self.menu.add( item )
+        
+        item = gtk.MenuItem( _('Delete group') )
+        item.connect( 'activate', self.on_menu_delete_group )
+        self.menu.add( item )
+        
+        self.menu.attach_to_widget(self, None)
+        self.menu.show_all()
+        
+    def show_click_column(self):
+        if len(self.get_columns()) == 1:
+            self.insert_column( self.click_column, 0)
+            
+    def hide_click_column(self):
+        if len(self.get_columns()) == 2:
+            self.remove_column( self.click_column )
+        
+    def on_toggle( self, cell, path, model ):
+        model[path][0] = not cell.get_active()
+        self.emit( 'changed', CHANGE_OTHER, None )
+        
+    def on_edit( self, cell, path, new_text, model ):
+        if new_text != "":
+            model[path][1] = new_text
+            self.emit( 'changed', CHANGE_OTHER, None )
+        
+    def on_menu_add_group( self, widget ):
+        '''Menu says add something'''
+        
+        input = dialogs.TextEntryDialog( _('New tag value?'), _('Enter new tag value'))
+        
+        if input.run() == gtk.RESPONSE_OK:
+            value = input.get_value()
+            
+            if value != "":
+                model = self.get_model()
+            
+                # Don't insert duplicate values
+                for row in model:
+                    if row[1] == value:
+                        row[0] = True
+                        self.emit( 'changed', CHANGE_OTHER, None )
+                        return
+            
+                model.append( [True, value] )
+                self.emit('changed', CHANGE_ADDED, value )
+        
+    def on_menu_delete_group( self, widget ):
+        '''Menu says delete something'''
+        
+        model, rows = self.get_selection().get_selected_rows()
+        iters = []
+        for row in rows:
+            iters.append( model.get_iter( row ) )
+        
+        for i in iters:
+            if i is not None:
+                self.emit( 'changed', CHANGE_DELETED, model.get_value(i, 1) )
+                model.remove(i)
+        
+    def on_mouse_press(self, widget, event):
+    
+        if event.button == 3:
+            
+            # TODO: select item before showing the menu for it
+            sel = self.get_selection()
+            
+            if sel.count_selected_rows() == 0:
+                info = self.get_path_at_pos( int(event.x), int(event.y) )
+                if info is not None:
+                    self.grab_focus()
+                    self.set_cursor( info[0], info[1], 0 )
+            
+            self.menu.popup( None, None, None, event.button, event.time )
+            return True
+            
+        return False
+    
+    def on_popup_menu(self, widget):
+        self.menu.popup( None, None, None, None, None )
+        return True
+
+gobject.type_register(GroupTaggerView)
+        
+class GroupTaggerModel(gtk.ListStore):
+    '''Model for tagger'''
+    
+    def __init__(self):
+        gtk.ListStore.__init__( self, gobject.TYPE_BOOLEAN, gobject.TYPE_STRING )
+        self.set_default_sort_func( self._sort_func )
+        self.set_sort_column_id( -1, gtk.SORT_DESCENDING )
+        
+    def _sort_func(self, model, iter1, iter2):
+        '''Sort the model first by enabled, then by group name'''
+    
+        v0_b, v0_s = self.get( iter1, 0, 1 )
+        v1_b, v1_s = self.get( iter2, 0, 1 )
+    
+        if v0_b == v1_b:
+            if v0_s == v1_s:
+                return 0
+            elif v0_s > v1_s:
+                return -1
+            
+            return 1
+        
+        elif v0_b is True:
+            return 1
+        
+        return -1
+        
+    def get_active_groups(self):
+        return [row[1] for row in self if row[0] == True ]
+        
+    def get_all_groups(self):
+        return [row[1] for row in self]
+            
+
+class GroupTaggerWidget(gtk.VBox):
+    '''Melds the tag view with an 'add' button'''
+
+    def __init__(self):
+        gtk.VBox.__init__(self)
+        
+        self.title = gtk.Label()
+        self.artist = gtk.Label()
+        self.view = GroupTaggerView( GroupTaggerModel() )
+        self.store = self.view.get_model()
+        
+        self.tag_button = gtk.Button( _('Add Group') )
+        self.tag_button.connect( 'clicked', self.on_add_tag_click )
+        
+        self.title.set_alignment(0,0.5)
+        self.title.set_line_wrap(True)
+        self.title.hide()
+        
+        self.artist.set_alignment(0,0.5)
+        self.artist.set_line_wrap(True)
+        self.artist.hide()
+        
+        scroll = gtk.ScrolledWindow()
+        scroll.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        scroll.set_shadow_type(gtk.SHADOW_IN)
+        scroll.add( self.view )
+        
+        self.pack_start( self.title, expand=False )
+        self.pack_start( self.artist, expand=False )
+        self.pack_start( scroll, True, True )
+        self.pack_start( self.tag_button, expand=False )
+        
+    def on_add_tag_click(self, widget):
+        self.view.on_menu_add_group( self.view )
+            
+    def set_title(self, title):
+        '''Sets the title for this widget. Hides title if title is None'''
+        if title is None:
+            self.title.hide()
+        else:
+            self.title.set_markup('<big><b>' + glib.markup_escape_text(title) + '</b></big>')
+            self.title.show()
+            
+    def set_artist(self, artist):
+        '''Sets the author for this widget. Hides it if author is None'''
+        if artist is None:
+            self.artist.hide()
+        else:
+            self.artist.set_text('by ' + artist)
+            self.artist.show()
+            
+    def set_track_info(self, track):
+        '''Shortcut for set_title/set_artist'''
+        if track is None:
+            self.set_title( None )
+            self.set_artist( None )
+        else:
+            self.set_title( track.get_tag_display( 'title' ) )
+            self.set_artist( track.get_tag_display( 'artist' ) )
+
+    def set_groups(self, groups):
+        '''
+            Accepts an array of tuples in the form (Boolean, String), where
+            the boolean is whether the group should be enabled, and string is
+            the name of the group
+        '''
+        self.view.freeze_child_notify()
+        self.view.set_model( None )
+        
+        self.store.clear()
+        for group in groups:
+            self.store.append( group )
+            
+        self.view.set_model( self.store )
+        self.view.thaw_child_notify()
+        
+
+        
+class GroupTaggerPanel(gtk.VBox):
+    '''A panel that has all of the functionality in it'''
+
+    def __init__(self):
+    
+        gtk.VBox.__init__(self)
+    
+        # add the tagger widget
+        self.tagger = GroupTaggerWidget()
+        
+        # add the widgets to this page
+        self.pack_start( self.tagger, expand=True ) 

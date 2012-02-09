9ec68859acb2156c5207f1450e4d3388c56a0b10
Brian Parma <execrable@gmail.com>
2012-02-09 14:26:14 -0700

    add preferences for ipv6 and server history

---
+++ plugins/daapclient/daapclientprefs.py
@@ -0,0 +1,15 @@
+from xlgui.preferences import widgets
+from xl.nls import gettext as _
+import os
+
+name = _('DAAP Client')
+basedir = os.path.dirname(os.path.realpath(__file__))
+ui = os.path.join(basedir, 'daapclient_prefs.ui')
+
+class Ipv6Preference(widgets.CheckPreference):
+    default = False
+    name = 'plugin/daapclient/ipv6'
+    
+class HistoryPreference(widgets.CheckPreference):
+    default = True
+    name = 'plugin/daapclient/history'

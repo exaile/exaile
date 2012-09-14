b1ec7eadb16722e102fc491ec3cbfb4f338a925d
dspicuzz <dustin@virtualroadside.com>
2012-09-14 01:33:43 -0400

    * Windows: Load the custom DirectSound plugin

--- xl/player/pipe.py
+++ xl/player/pipe.py
@@ -27,6 +27,8 @@
 
 import copy
 import logging
+import os.path
+import sys
 
 import pygst
 pygst.require('0.10')
@@ -335,6 +337,42 @@ SINK_PRESETS = {
             "pipe"      : "directsoundsink"
         }
 }
+
+def __load_exaile_directsound_plugin():
+    '''
+        The default GST DirectSound plugin does not support selecting
+        an output device via the property probe mechanism. For plugins
+        like the Preview Device, this is critical. 
+        
+        This should be removed once GST has proper support in mainline.
+    '''
+    import glib
+
+    try:
+        plugin_path = os.path.abspath(os.path.join(__file__, '../../../tools/win-installer/libgstexailedirectsoundsink.dll'))
+        plugin = gst.plugin_load_file(plugin_path)
+        gst.registry_get_default().add_plugin(plugin)
+        
+    except glib.GError, e:
+        logger.error("Error loading custom DirectSound plugin: %s" % str(e))
+        
+    else:
+        # add to presets if successfully loaded
+        preset = {
+            "name"      : "DirectSound (Exaile %s)" % _('Custom'),
+            "pipe"      : "exailedirectsoundsink"
+        }
+        
+        SINK_PRESETS["exailedirectsound"] = preset
+        
+        # make this default if there is no default
+        if settings.get_option('player/audiosink', None) == None:
+            settings.set_option('player/audiosink', 'exailedirectsound')
+    
+    
+if sys.platform == 'win32':
+    __load_exaile_directsound_plugin()
+    
 
 def sink_from_preset(player, preset):
     if preset == "custom":

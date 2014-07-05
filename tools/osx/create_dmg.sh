66e54652a2c8675e5a218db9609764626834f612
Dustin Spicuzza <dustin@virtualroadside.com>
2014-07-05 02:21:00 -0400

    Update OSX builder to use a clean checkout

--- tools/osx/create_dmg.sh
+++ tools/osx/create_dmg.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-cd $(dirname "$0")
-rm -rf build Exaile Exaile.dmg
+cd $(dirname "$0")/../..
+rm -rf dist/copy
 
+mkdir -p dist
+bzr export dist/copy
+
+pushd dist/copy/tools/osx
 python setup.py py2app
 
 cp ../../README.OSX Exaile/README.txt
 ln -s /Applications Exaile/Applications
 hdiutil create -srcfolder Exaile Exaile.dmg
+
+popd
+mv dist/copy/tools/osx/Exaile.dmg dist
+
+rm -rf dist/copy

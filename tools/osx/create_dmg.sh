e1c71c4dc02a19fd42f305d28b9ca91522272e68
Dustin Spicuzza <dustin@virtualroadside.com>
2014-07-05 23:59:10 -0400

    Include version in dmg output

--- tools/osx/create_dmg.sh
+++ tools/osx/create_dmg.sh
@@ -7,6 +7,10 @@ mkdir -p dist
 bzr export dist/copy
 
 pushd dist/copy/tools/osx
+
+EXAILE_DIR=`dirname $0`/../..
+DIST_VERSION=`EXAILE_DIR=$EXAILE_DIR python2 -c 'import xl.xdg; xl.xdg.local_hack=False; import xl.version; print xl.version.__version__'`
+
 python setup.py py2app
 
 cp ../../README.OSX Exaile/README.txt
@@ -14,6 +18,6 @@ ln -s /Applications Exaile/Applications
 hdiutil create -srcfolder Exaile Exaile.dmg
 
 popd
-mv dist/copy/tools/osx/Exaile.dmg dist
+mv dist/copy/tools/osx/Exaile.dmg dist/exaile-$DIST_VERSION.dmg
 
 rm -rf dist/copy

exaile/exaile

commit 9744148039a73e2d9e3608dcab8cfa711cc595a6

Remove most GtkStock items in xlgui/
GtkStock is deprecated. This commit removes most usages of GtkStock.

xlgui/widgets/menuitems.py
@@ -96,7 +96,7 @@ def _enqueue_cb(widget, name, parent, context, get_tracks_func):
     player.QUEUE.extend(tracks)
 
 def EnqueueMenuItem(name, after, get_tracks_func=generic_get_tracks_func):
-    return menu.simple_menu_item(name, after, _("Enqueue"), Gtk.STOCK_ADD,
+    return menu.simple_menu_item(name, after, _("Enqueue"), 'list-add',
             _enqueue_cb, callback_args=[get_tracks_func])
 
 # TODO: move logic into (GUI?) playlist
@@ -124,7 +124,7 @@ def ReplaceCurrentMenuItem(name, after, get_tracks_func=generic_get_tracks_func)
 
 def AppendMenuItem(name, after, get_tracks_func=generic_get_tracks_func):
     return menu.simple_menu_item(name, after, _("Append to Current"),
-            'gtk-add', _append_cb, callback_args=[get_tracks_func])
+            'list-add', _append_cb, callback_args=[get_tracks_func])
 
 def _properties_cb(widget, name, parent, context, get_tracks_func, dialog_parent):
     tracks = get_tracks_func(parent, context)
@@ -134,7 +134,7 @@ def _properties_cb(widget, name, parent, context, get_tracks_func, dialog_parent
 def PropertiesMenuItem(name, after, get_tracks_func=generic_get_tracks_func,
         dialog_parent=None):
     return menu.simple_menu_item(name, after, None,
-            'gtk-properties', _properties_cb,
+            'document-properties', _properties_cb,
             callback_args=[get_tracks_func, dialog_parent])
 
 
@@ -147,7 +147,7 @@ def _open_directory_cb(widget, name, parent, context, get_tracks_func):
 
 def OpenDirectoryMenuItem(name, after, get_tracks_func=generic_get_tracks_func):
     return menu.simple_menu_item(name, after, _("Open Directory"),
-            'gtk-open', _open_directory_cb, callback_args=[get_tracks_func])
+            'folder-open', _open_directory_cb, callback_args=[get_tracks_func])
 
 def generic_trash_tracks_func(parent, context, tracks):
     for track in tracks:
@@ -195,7 +195,7 @@ class ShowCurrentTrackMenuItem(menu.MenuItem):
     def __init__(self, name, after, callback=None, callback_args=[], accelerator=None):
         def factory(container, parent, context):
             item = Gtk.ImageMenuItem.new_with_mnemonic(_("_Show Playing Track"))
-            image = Gtk.Image.new_from_icon_name('gtk-jump-to-ltr',
+            image = Gtk.Image.new_from_icon_name('go-jump',
                     size=Gtk.IconSize.MENU)
             item.set_image(image)
 
@@ -221,25 +221,25 @@ class ShowCurrentTrackMenuItem(menu.MenuItem):
 # get_pl_func kwarg
 
 def RenamePlaylistMenuItem(name, after, get_pl_func=generic_get_playlist_func):
-    return menu.simple_menu_item(name, after, _('Rename'), 'gtk-edit',
+    return menu.simple_menu_item(name, after, _('Rename'), 'accessories-text-editor',
                           lambda w, n, o, c: o.rename_playlist(get_pl_func(o, c)),
                           condition_fn=lambda n, p, c: not isinstance(c['selected-playlist'], playlist.SmartPlaylist))
     
 def EditPlaylistMenuItem(name, after, get_pl_func=generic_get_playlist_func):
-    return menu.simple_menu_item(name, after, _('Edit'), 'gtk-edit',
+    return menu.simple_menu_item(name, after, _('Edit'), 'accessories-text-editor',
                           lambda w, n, o, c: o.edit_smart_playlist(get_pl_func(o, c)),
                           condition_fn=lambda n, p, c: isinstance(c['selected-playlist'], playlist.SmartPlaylist))
 
 def ExportPlaylistMenuItem(name, after, get_pl_func=generic_get_playlist_func):
-    return menu.simple_menu_item(name, after, _('Export Playlist'), 'gtk-save',
+    return menu.simple_menu_item(name, after, _('Export Playlist'), 'document-save-as',
                           lambda w, n, o, c: dialogs.export_playlist_dialog(get_pl_func(o, c)))
 
 def ExportPlaylistFilesMenuItem(name, after, get_pl_func=generic_get_playlist_func):
-    return menu.simple_menu_item(name, after, _('Export Files'), 'gtk-save',
+    return menu.simple_menu_item(name, after, _('Export Files'), 'document-save-as',
                           lambda w, n, o, c: dialogs.export_playlist_files(get_pl_func(o, c)))
 
 def DeletePlaylistMenuItem(name, after, get_pl_func=generic_get_playlist_func):
-    return menu.simple_menu_item(name, after, _('Delete Playlist'), 'gtk-delete',
+    return menu.simple_menu_item(name, after, _('Delete Playlist'), 'edit-delete',
                           lambda w, n, o, c: o.remove_playlist(get_pl_func(o, c)))
     
 

xlgui/main.py
@@ -416,11 +416,11 @@ class MainWindow(GObject.GObject):
         """
         widget.__hovered = True
         if event.get_state() & Gdk.ModifierType.SHIFT_MASK:
-            widget.set_image(Gtk.Image.new_from_stock(
-                Gtk.STOCK_STOP, Gtk.IconSize.BUTTON))
+            widget.set_image(Gtk.Image.new_from_icon_name(
+                'process-stop', Gtk.IconSize.BUTTON))
         else:
-            widget.set_image(Gtk.Image.new_from_stock(
-                Gtk.STOCK_MEDIA_STOP, Gtk.IconSize.BUTTON))
+            widget.set_image(Gtk.Image.new_from_icon_name(
+                'media-playback-stop', Gtk.IconSize.BUTTON))
 
     def on_stop_button_leave_notify_event(self, widget, event):
         """
@@ -429,16 +429,16 @@ class MainWindow(GObject.GObject):
         widget.__hovered = False
         if not widget.is_focus() and \
            ~(event.get_state() & Gdk.ModifierType.SHIFT_MASK):
-            widget.set_image(Gtk.Image.new_from_stock(
-                Gtk.STOCK_MEDIA_STOP, Gtk.IconSize.BUTTON))
+            widget.set_image(Gtk.Image.new_from_icon_name(
+                'media-playback-stop', Gtk.IconSize.BUTTON))
 
     def on_stop_button_key_press_event(self, widget, event):
         """
             Shows SPAT icon on Shift key press
         """
         if event.keyval in (Gdk.KEY_Shift_L, Gdk.KEY_Shift_R):
-            widget.set_image(Gtk.Image.new_from_stock(
-                Gtk.STOCK_STOP, Gtk.IconSize.BUTTON))
+            widget.set_image(Gtk.Image.new_from_icon_name(
+                'process-stop', Gtk.IconSize.BUTTON))
             widget.set_data('toggle_spat', True)
 
         if event.keyval in (Gdk.KEY_space, Gdk.KEY_Return):
@@ -452,8 +452,8 @@ class MainWindow(GObject.GObject):
             Resets the button icon
         """
         if event.keyval in (Gdk.KEY_Shift_L, Gdk.KEY_Shift_R):
-            widget.set_image(Gtk.Image.new_from_stock(
-                Gtk.STOCK_MEDIA_STOP, Gtk.IconSize.BUTTON))
+            widget.set_image(Gtk.Image.new_from_icon_name(
+                'media-playback-stop', Gtk.IconSize.BUTTON))
             widget.set_data('toggle_spat', False)
 
     def on_stop_button_focus_out_event(self, widget, event):
@@ -462,8 +462,8 @@ class MainWindow(GObject.GObject):
             the button is still hovered
         """
         if not getattr(widget, '__hovered', False):
-            widget.set_image(Gtk.Image.new_from_stock(
-                Gtk.STOCK_MEDIA_STOP, Gtk.IconSize.BUTTON))
+            widget.set_image(Gtk.Image.new_from_icon_name(
+                'media-playback-stop', Gtk.IconSize.BUTTON))
 
     def on_stop_button_press_event(self, widget, event):
         """
@@ -476,7 +476,7 @@ class MainWindow(GObject.GObject):
             menu = guiutil.Menu()
             menu.append(_("Toggle: Stop after Selected Track"),
                 self.on_spat_clicked,
-                Gtk.STOCK_STOP)
+                'process-stop')
             menu.popup(None, None, None, None, event.button, event.time)
 
     def on_stop_button_release_event(self, widget, event):
@@ -493,15 +493,15 @@ class MainWindow(GObject.GObject):
         """
         target = widget.drag_dest_find_target(context, widget.drag_dest_get_target_list())
         if target == 'exaile-index-list':
-            widget.set_image(Gtk.Image.new_from_stock(
-                Gtk.STOCK_STOP, Gtk.IconSize.BUTTON))
+            widget.set_image(Gtk.Image.new_from_icon_name(
+                'process-stop', Gtk.IconSize.BUTTON))
 
     def on_stop_button_drag_leave(self, widget, context, time):
         """
             Resets the stop button
         """
-        widget.set_image(Gtk.Image.new_from_stock(
-            Gtk.STOCK_MEDIA_STOP, Gtk.IconSize.BUTTON))
+        widget.set_image(Gtk.Image.new_from_icon_name(
+            'media-playback-stop', Gtk.IconSize.BUTTON))
 
     def on_stop_button_drag_data_received(self, widget, context, x, y, selection, info, time):
         """
@@ -634,11 +634,11 @@ class MainWindow(GObject.GObject):
             already begun
         """
         if player.is_paused():
-            image = Gtk.Image.new_from_stock(Gtk.STOCK_MEDIA_PLAY,
+            image = Gtk.Image.new_from_icon_name('media-playback-start',
                 Gtk.IconSize.SMALL_TOOLBAR)
             tooltip = _('Continue Playback')
         else:
-            image = Gtk.Image.new_from_stock(Gtk.STOCK_MEDIA_PAUSE,
+            image = Gtk.Image.new_from_icon_name('media-playback-pause',
                 Gtk.IconSize.SMALL_TOOLBAR)
             tooltip = _('Pause Playback')
 
@@ -817,7 +817,7 @@ class MainWindow(GObject.GObject):
 
         self._update_track_information()
         GLib.idle_add(self.playpause_button.set_image,
-            Gtk.Image.new_from_stock(Gtk.STOCK_MEDIA_PAUSE,
+            Gtk.Image.new_from_icon_name('media-playback-pause',
             Gtk.IconSize.SMALL_TOOLBAR))
         GLib.idle_add(self.playpause_button.set_tooltip_text,
             _('Pause Playback'))
@@ -829,7 +829,7 @@ class MainWindow(GObject.GObject):
         GLib.idle_add(self.window.set_title, 'Exaile')
 
         GLib.idle_add(self.playpause_button.set_image,
-            Gtk.Image.new_from_stock(Gtk.STOCK_MEDIA_PLAY,
+            Gtk.Image.new_from_icon_name('media-playback-start',
             Gtk.IconSize.SMALL_TOOLBAR))
         GLib.idle_add(self.playpause_button.set_tooltip_text,
             _('Start Playback'))

xlgui/panel/collection.py
@@ -335,8 +335,8 @@ class CollectionPanel(panel.Panel):
             'artist', Gtk.IconSize.SMALL_TOOLBAR)
         self.date_image = icons.MANAGER.pixbuf_from_icon_name(
             'office-calendar', Gtk.IconSize.SMALL_TOOLBAR)
-        self.album_image = icons.MANAGER.pixbuf_from_stock(
-            Gtk.STOCK_CDROM, Gtk.IconSize.SMALL_TOOLBAR)
+        self.album_image = icons.MANAGER.pixbuf_from_icon_name(
+            'image-x-generic', Gtk.IconSize.SMALL_TOOLBAR)
         self.title_image = icons.MANAGER.pixbuf_from_icon_name(
             'audio-x-generic', Gtk.IconSize.SMALL_TOOLBAR)
         self.genre_image = icons.MANAGER.pixbuf_from_icon_name(

xlgui/panel/device.py
@@ -134,7 +134,7 @@ class DevicePanel(panel.Panel):
         thread = DeviceTransferThread(self.device)
         thread.connect('done', lambda *e: self.load_tree())
         self.main.controller.progress_manager.add_monitor(thread,
-                _("Transferring to %s...") % self.name, Gtk.STOCK_GO_UP)
+                _("Transferring to %s...") % self.name, 'go-up')
 
     def get_panel(self):
         return self.collectionpanel.get_panel()

xlgui/panel/menus.py
@@ -171,17 +171,17 @@ def __create_playlist_panel_menus():
     
     # w, n, o, c: window, name, parent, context
 
-    menu.simple_menu_item('new-playlist', [], _('New Playlist'), 'gtk-new',
+    menu.simple_menu_item('new-playlist', [], _('New Playlist'), 'tab-new',
                           lambda w, n, o, c: o.add_new_playlist()) \
         .register('playlist-panel-menu')
         
     menu.simple_menu_item('new-smart-playlist', ['new-playlist'],
-                          _('New Smart Playlist'), 'gtk-new',
+                          _('New Smart Playlist'), 'tab-new',
                          lambda w, n, o, c: o.add_smart_playlist()) \
         .register('playlist-panel-menu')
         
     menu.simple_menu_item('import-playlist', ['new-smart-playlist'],
-                          _('Import Playlist'), 'gtk-open',
+                          _('Import Playlist'), 'document-open',
                          lambda w, n, o, c: o.import_playlist()) \
         .register('playlist-panel-menu')
         
@@ -248,7 +248,7 @@ def __create_radio_panel_menus():
     
     # w, n, o, c: window, name, parent, context
 
-    menu.simple_menu_item('new-station', [], _('New Station'), 'gtk-new',
+    menu.simple_menu_item('new-station', [], _('New Station'), 'list-add',
                           lambda w, n, o, c: o._on_add_button_clicked()) \
         .register('radio-panel-menu')
 

xlgui/panel/radio.py
@@ -248,8 +248,8 @@ class RadioPanel(panel.Panel, playlistpanel.BasePlaylistPanelMixin):
             'audio-x-generic', Gtk.IconSize.SMALL_TOOLBAR)
         self.folder = self.tree.render_icon(
             Gtk.STOCK_DIRECTORY, Gtk.IconSize.SMALL_TOOLBAR)
-        self.refresh_image = icons.MANAGER.pixbuf_from_stock(
-            Gtk.STOCK_REFRESH)
+        self.refresh_image = icons.MANAGER.pixbuf_from_icon_name(
+            'view-refresh')
 
         self.custom = self.model.append(None, [self.folder, _("Saved Stations"), None])
         self.radio_root = self.model.append(None, [self.folder, _("Radio "

xlgui/playlist_container.py
@@ -129,7 +129,8 @@ class PlaylistNotebook(SmartNotebook):
         item = menu.simple_separator('clear-sep',[])
         item.register('playlist-closed-tab-menu', self)
         
-        item = menu.simple_menu_item('clear-history', ['clear-sep'], None, 'gtk-clear',
+        item = menu.simple_menu_item('clear-history', ['clear-sep'], 
+            _("_Clear Tab History"), 'edit-clear-all',
             self.clear_closed_tabs)
         item.register('playlist-closed-tab-menu', self)     
             

xlgui/widgets/dialogs.py
@@ -203,15 +203,15 @@ class MultiTextEntryDialog(Gtk.Dialog):
                 for field in self.fields:
                     if len(field.get_text()) > 0:
                         # Unset possible previous marks
-                        field.set_icon_from_stock(
+                        field.set_icon_from_icon_name(
                             Gtk.EntryIconPosition.SECONDARY,
                             None
                         )
                     else:
                         # Mark via warning
-                        field.set_icon_from_stock(
+                        field.set_icon_from_icon_name(
                             Gtk.EntryIconPosition.SECONDARY,
-                            Gtk.STOCK_DIALOG_WARNING
+                            'dialog-warning'
                         )
         self.hide()
 

commit 7cc6b42b69439e111542c706acc29423c3656a7b

Fix bug introduced in previous commit 9744148039a73e2d9e3608dcab8cfa711cc595a6

xlgui/widgets/menuitems.py
@@ -135,7 +135,7 @@ def _properties_cb(widget, name, parent, context, get_tracks_func, dialog_parent
 
 def PropertiesMenuItem(name, after, get_tracks_func=generic_get_tracks_func,
         dialog_parent=None):
-    return menu.simple_menu_item(name, after, None,
+    return menu.simple_menu_item(name, after, _("_Track Properties"),
             'document-properties', _properties_cb,
             callback_args=[get_tracks_func, dialog_parent])
 
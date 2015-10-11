exaile/exaile

commit b9e56ef5eaee6efd4be896992ecd08efbb4d6f87

lyricsmania: Fix Unicode error.

plugins/lyricsmania/__init__.py
@@ -82,4 +82,5 @@ class LyricsMania(LyricSearchMethod):
         except :
             raise LyricsNotFoundException
 
+        lyrics = lyrics.decode('utf-8', errors='replace')
         return (lyrics, self.name, url)

commit 109a6367ad838e2052a804c3ac1f002543609c77

lyricsmania: Fix more Unicode problems.
Should fix the regression mentioned in
https://github.com/exaile/exaile/commit/b9e56ef5eaee6efd4be896992ecd08efbb4d6f87#commitcomment-13704889

plugins/lyricsmania/__init__.py
@@ -82,5 +82,8 @@ class LyricsMania(LyricSearchMethod):
         except :
             raise LyricsNotFoundException
 
-        lyrics = lyrics.decode('utf-8', errors='replace')
+        # We end up with unicode in some systems, str (bytes) in others;
+        # no idea why and which one is correct.
+        if isinstance(lyrics, bytes):
+            lyrics = lyrics.decode('utf-8', errors='replace')
         return (lyrics, self.name, url)
b1675ae53b7ea57554b76dfe4baf5e51c828a44f
Johannes Sasongko <sasongko@gmail.com>
2015-10-27 02:10:41 +1000

    _matroska: Update copyright year.

--- xl/metadata/_matroska.py
+++ xl/metadata/_matroska.py
@@ -1,5 +1,5 @@
 # EBML/Matroska parser
-# Copyright (C) 2010  Johannes Sasongko <sasongko@gmail.com>
+# Copyright (C) 2010, 2015  Johannes Sasongko <sasongko@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

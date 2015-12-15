afdbc254f2e59fbb3882afdc62c3586366b3d467
Dustin Spicuzza <dustin@virtualroadside.com>
2015-12-14 22:26:06 -0500

    Unify plugin versions and add tools to manage it
    - Fixes #82

--- tools/dist.sh
+++ tools/dist.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-EXAILE_DIR=`dirname $0`/..
+THISDIR=`dirname $0`
+EXAILE_DIR="$THISDIR"/..
 DIST_VERSION=`EXAILE_DIR=$EXAILE_DIR python2 -c 'import xl.xdg; xl.xdg.local_hack=False; import xl.version; print xl.version.__version__'` 
+
+if ! "$THISDIR"/plugin_tool.py check; then
+    if [ -z "$EXAILE_IGNORE_PLUGINS" ]; then
+        echo "Plugin version check failed, set environment variable EXAILE_IGNORE_PLUGINS=1 to ignore"
+        exit 1
+    fi
+fi
 
 if [ ! -f "tools/installer/build_win32_installer.sh" ]; then
   echo "python-gtk3-gst-sdk links not installed (use create_links.sh)! Cannot build windows installer"
---
+++ tools/plugin_tool.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import re
+import os
+from os.path import abspath, dirname, join, exists
+import sys
+
+v_re1 = re.compile(r"^Version='(.*)'$")
+v_re2 = re.compile(r'^Version="(.*)"$')
+
+def _find_bad_plugin_versions(plugins_dir):
+    for pname in os.listdir(plugins_dir):
+        p = join(plugins_dir, pname, 'PLUGININFO')
+        if exists(p):
+            with open(p) as fp:
+                contents = fp.read()
+                
+            s = contents.splitlines()
+            for i, l in enumerate(s):
+                m = v_re1.match(l) or v_re2.match(l)
+                if m:
+                    vv = m.group(1)
+                    if vv != v:
+                        yield pname, p, vv, s[:i] + s[i+1:]
+                    
+                    break
+            else:
+                print("Warning: no version found in %s" % pname)
+
+def check(v, plugins):
+    retval = 0
+    for pname, _, vv, _ in plugins:
+        print("ERROR: Plugin %s version is %s, not %s" % (pname, vv, v))
+        retval = 1
+    
+    return retval
+
+def fixversion(v, plugins):
+    v = "Version='%s'" % v
+    for _, p, _, contents in plugins:
+        contents = '\n'.join([v] + contents) + '\n'
+        with open(p, 'w') as fp:
+            fp.write(contents)
+
+
+if __name__ == '__main__':
+    exaile_dir = abspath(join(dirname(__file__), '..'))
+    plugins_dir = join(exaile_dir, 'plugins')
+    
+    os.environ['EXAILE_DIR'] = exaile_dir
+    sys.path.insert(0, exaile_dir)
+    
+    import xl.xdg
+    xl.xdg.local_hack = False
+    import xl.version
+    v = '%s.%s' % (xl.version.major, xl.version.minor)
+    
+    arg = sys.argv[1] if len(sys.argv) > 1 else ''
+    
+    if arg == 'check':
+        retval = check(v, _find_bad_plugin_versions(plugins_dir))
+    elif arg == 'fix':
+        retval = fixversion(v, _find_bad_plugin_versions(plugins_dir))
+    else:
+        print('Usage: %s check|fix' % sys.argv[0])
+        retval = 1
+        
+    exit(retval)

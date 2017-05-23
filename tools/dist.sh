67fb55d129d43248c3c89558922bb006a7466502
Dustin Spicuzza <dustin@virtualroadside.com>
2017-05-23 02:50:50 -0400

    Update installer stuff

--- tools/dist.sh
+++ tools/dist.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 THISDIR=`dirname $0`
 EXAILE_DIR="$THISDIR"/..
 DIST_VERSION=`EXAILE_DIR=$EXAILE_DIR python2 -c 'import xl.xdg; xl.xdg.local_hack=False; import xl.version; print xl.version.__version__'` 
@@ -11,10 +13,10 @@ if ! "$THISDIR"/plugin_tool.py check; then
     fi
 fi
 
-if [ ! -f "tools/installer/build_win32_installer.sh" ]; then
-  echo "python-gtk3-gst-sdk links not installed (use create_links.sh)! Cannot build windows installer"
-  exit 1
-fi
+#if [ ! -f "tools/installer/build_win32_installer.sh" ]; then
+#  echo "python-gtk3-gst-sdk links not installed (use create_links.sh)! Cannot build windows installer"
+#  exit 1
+#fi
 
 echo "Creating distribution for Exaile $DIST_VERSION"
 
@@ -27,21 +29,21 @@ tar --gzip --format=posix --owner 0 --group 0 \
 # See tools/installer/README.md for instructions
 #
 
-echo "Generating Windows installer via python-gtk3-gst-sdk"
+#echo "Generating Windows installer via python-gtk3-gst-sdk"
 
-pushd tools/installer
-./build_win32_installer.sh
+#pushd tools/installer
+#./build_win32_installer.sh
 
-if [ "$?" != "0" ]; then
-    echo "Warning: the win32 installer build seems to have failed..."
-fi
+#if [ "$?" != "0" ]; then
+#    echo "Warning: the win32 installer build seems to have failed..."
+#fi
 
-popd 
+#popd 
 
-mv tools/installer/exaile-LATEST.exe dist/exaile-${DIST_VERSION}.exe
+#mv tools/installer/exaile-LATEST.exe dist/exaile-${DIST_VERSION}.exe
 
 echo "Successfully built exaile installer! Going to sign the resulting packages"
 echo "with gpg (feel free to CTRL-C at this point if you don't care about that)"
 
 gpg --armor --sign --detach-sig dist/exaile-${DIST_VERSION}.tar.gz
-gpg --armor --sign --detach-sig dist/exaile-${DIST_VERSION}.exe
+#gpg --armor --sign --detach-sig dist/exaile-${DIST_VERSION}.exe

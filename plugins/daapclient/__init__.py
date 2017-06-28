cd1d092af782492122380f817cbc72a053cf9e04
Christian Stadelmann <dev@genodeftest.de>
2017-06-28 19:43:29 +0200

    plugins/daapclient: reflect syntax change
    Tuple parameter unpacking was removed from python 3 (See PEP 3113).
    
    This is basically commit acd988dfb36d51ac481f8704a5079355e7b3c04c
    by @mexicarne (Ruslan Makhmatkhanov). Thanks!

--- plugins/daapclient/__init__.py
+++ plugins/daapclient/__init__.py
@@ -311,12 +311,14 @@ class DaapManager:
 
         self.history = DaapHistory(5, menu=hmenu, callback=self.connect_share)
 
-    def connect_share(self, obj, (name, address, port, svc)):
+    def connect_share(self, obj, args):
         '''
             This function is called when a user wants to connec to
         a DAAP share.  It creates a new panel for the share, and
         requests a track list.
+            `args` is a tuple of (name, address, port, service)
         '''
+        name, address, port, svc = args  # unpack tuple
         conn = DaapConnection(name, address, port)
 
         conn.connect()

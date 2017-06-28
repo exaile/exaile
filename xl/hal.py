exaile/exaile

commit b4a251e4ce373047a81b9fb452fc4b3b76dac0a1

xl/hal: Don't print non-fatal traceback
Failure to connect to UDisks is no big deal as it does not break any important
functionality in Exaile. This change decreases the log level the traceback is
reported on.

Fixes #400.

xl/hal.py
@@ -145,8 +145,9 @@ class UDisksBase(providers.ProviderHandler):
             logger.info("Connected to %s", self.name)
             event.log_event("hal_connected", self, None)
         except Exception:
-            logger.exception("Failed to connect to %s, "
-                             "autodetection of devices will be disabled.", self.name)
+            logger.info("Failed to connect to %s, "
+                        "autodetection of devices will be disabled.",
+                        self.name, {'exc_info': 1})
             return False
 
         self._state = 'addremove'

commit 578514f22b93cdedce5151cc474897e1711b883b

xl/hal: Fix log message
I broke this in b4a251e4ce373047a81b9fb452fc4b3b76dac0a1, so I fix it now.

xl/hal.py
@@ -145,9 +145,8 @@ class UDisksBase(providers.ProviderHandler):
             logger.info("Connected to %s", self.name)
             event.log_event("hal_connected", self, None)
         except Exception:
-            logger.info("Failed to connect to %s, "
-                        "autodetection of devices will be disabled.",
-                        self.name, {'exc_info': 1})
+            logger.info("Failed to connect to %s, automatic detection of "
+                        "devices will be disabled.", self.name)
             return False
 
         self._state = 'addremove'
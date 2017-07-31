exaile/exaile

commit 26b9961b69b066e59e44a918af9ba2b3703b08de

Plugins: Don't access GUI before it is loaded

plugins/desktopcover/__init__.py
@@ -58,26 +58,33 @@ def __migrate_anchor_setting():
         settings.set_option('plugin/desktopcover/anchor', gravity)
 
 
-def enable(exaile):
-    """
-        Enables the desktop cover plugin
-    """
-    __migrate_anchor_setting()
+class DesktopCoverPlugin(object):
 
-    global DESKTOPCOVER
-    DESKTOPCOVER = DesktopCover()
+    def __init__(self):
+        self.__desktop_cover = None
 
+    def enable(self, _exaile):
+        """
+            Enables the plugin
+        """
+        __migrate_anchor_setting()
 
-def disable(exaile):
-    """
-        Disables the desktop cover plugin
-    """
-    global DESKTOPCOVER
-    DESKTOPCOVER.destroy()
+    def on_gui_loaded(self):
+        self.__desktop_cover = DesktopCover()
+
+    def disable(self, _exaile):
+        """
+            Disables the desktop cover plugin
+        """
+        self.__desktop_cover.destroy()
+        self.__desktop_cover = None
 
 
 def get_preferences_pane():
     return desktopcover_preferences
+
+
+plugin_class = DesktopCoverPlugin
 
 
 class DesktopCover(Gtk.Window):

plugins/abrepeat/__init__.py
@@ -21,25 +21,31 @@ from xl import event, player, providers
 from xl.nls import gettext as _
 from xlgui.widgets import playback
 
-MENU_ITEM = None
+
+class ABRepeatPlugin(object):
+
+    def __init__(self):
+        self.__menu_item = None
+
+    def enable(self, _exaile):
+        """
+            Enables the plugin
+        """
+        pass  # needs to be implemented, otherwise xl.plugins will break
+
+    def on_gui_loaded(self):
+        self.__menu_item = RepeatSegmentMenuItem()
+        providers.register('progressbar-context-menu', self.__menu_item)
+
+    def disable(self, _exaile):
+        """
+            Disables the plugin
+        """
+        self.__menu_item.destroy()
+        providers.unregister('progressbar-context-menu', self.__menu_item)
 
 
-def enable(exaile):
-    """
-        Enables the plugin
-    """
-    global MENU_ITEM
-    MENU_ITEM = RepeatSegmentMenuItem()
-    providers.register('progressbar-context-menu', MENU_ITEM)
-
-
-def disable(exaile):
-    """
-        Disables the plugin
-    """
-    global MENU_ITEM
-    MENU_ITEM.destroy()
-    providers.unregister('progressbar-context-menu', MENU_ITEM)
+plugin_class = ABRepeatPlugin
 
 
 class RepeatSegmentMenuItem(playback.MoveMarkerMenuItem,

commit ca97bfd864b0d43c44410a20e5ff5409b887cd58

Plugin desktopcover: Fix bug calling migration code
This bug was introduced in commit 26b9961b69b066e59e44a918af9ba2b3703b08de.

plugins/desktopcover/__init__.py
@@ -38,25 +38,6 @@ import desktopcover_preferences
 DESKTOPCOVER = None
 
 
-def __migrate_anchor_setting():
-    """
-        Migrates gravity setting from the old
-        integer values to the new string values
-    """
-    gravity = settings.get_option('plugin/desktopcover/anchor', 'topleft')
-    gravity_map = DesktopCover.gravity_map
-
-    if gravity not in gravity_map:
-        gravities = gravity_map.keys()
-
-        try:
-            gravity = gravities[gravity]
-        except (IndexError, TypeError):
-            gravity = 'topleft'
-
-        settings.set_option('plugin/desktopcover/anchor', gravity)
-
-
 class DesktopCoverPlugin(object):
 
     def __init__(self):
@@ -66,7 +47,7 @@ class DesktopCoverPlugin(object):
         """
             Enables the plugin
         """
-        __migrate_anchor_setting()
+        self.__migrate_anchor_setting()
 
     def on_gui_loaded(self):
         self.__desktop_cover = DesktopCover()
@@ -77,6 +58,25 @@ class DesktopCoverPlugin(object):
         """
         self.__desktop_cover.destroy()
         self.__desktop_cover = None
+
+    @staticmethod
+    def __migrate_anchor_setting():
+        """
+            Migrates gravity setting from the old
+            integer values to the new string values
+        """
+        gravity = settings.get_option('plugin/desktopcover/anchor', 'topleft')
+        gravity_map = DesktopCover.gravity_map
+
+        if gravity not in gravity_map:
+            gravities = gravity_map.keys()
+
+            try:
+                gravity = gravities[gravity]
+            except (IndexError, TypeError):
+                gravity = 'topleft'
+
+            settings.set_option('plugin/desktopcover/anchor', gravity)
 
 
 def get_preferences_pane():
91b4a554fd5402249fcbf21ed6d3bea4228c2776
Johannes Sasongko <sasongko@gmail.com>
2018-06-13 20:13:14 +1000

    generate-completion: Update copyright year for my recent change.

--- tools/generate-completion.py
+++ tools/generate-completion.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 
-# Copyright (C) 2015, 2017  Johannes Sasongko <sasongko@gmail.com>
+# Copyright (C) 2015, 2017-2018  Johannes Sasongko <sasongko@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

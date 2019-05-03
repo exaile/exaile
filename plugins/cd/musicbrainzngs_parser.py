1e0855fa01d39639613c21929805bead94b5b45f
Christian Stadelmann <dev@genodeftest.de>
2019-05-04 01:41:39 +0200

    plugin cd: Improve musicbrainzngs support
    musicbrainzngs_parser.py:
    * Add links to documentation
    * Simplify user agent initialization
    * Fetch more fields from musicbrainz:
      * isrcs -> improve detection of right metadata set
      * artist-credits -> include track specific artists or multiple artists
    * improve parser syntax for readability
    * change log level, remove some debugging code and comments
    * fetch cover image

--- plugins/cd/musicbrainzngs_parser.py
+++ plugins/cd/musicbrainzngs_parser.py
@@ -4,43 +4,56 @@
     It uses the online database provided by musicbrainzngs.
     
     musicbrainz documentation:
+    * Wiki: https://wiki.musicbrainz.org/
     * Web API: https://musicbrainz.org/doc/Development/XML%20Web%20Service/Version%202
     * Web API format: https://github.com/metabrainz/mmd-schema/blob/master/schema/musicbrainz_mmd-2.0.rng
+    * Database schema: https://wiki.musicbrainz.org/MusicBrainz_Database/Schema
+    
+    musicbrainzngs documentation:
+    * https://python-musicbrainzngs.readthedocs.io/
     
     Note: All the "priority" values are arbitrary/random choices and may need improvement
+    
+    TODO: Room for improvement:
+    * fetch composers (especially interesting for classical music)
+    * test text-representation for non-latin languages
+    * get more data with secondary query, e.g. genre ("medium_tags") 
+        see https://wiki.musicbrainz.org/Genre
 """
 
 
 from __future__ import division
 
+import json
 import logging
+import urllib2
 
 import musicbrainzngs
 
 from xl import main
+from xl.metadata import CoverImage
 from xl.trax import Track
+
+from xl.covers import MANAGER as CoverManager
+
 from plugins.cd import discid_parser
 
 
 logger = logging.getLogger(__name__)
 
 
-MUSICBRAINZNGS_INITIALIZED = False
+def __init_musicbrainzngs():
+    version = main.exaile().get_user_agent_for_musicbrainz()
+    musicbrainzngs.set_useragent(*version)
 
-# TODO which fields are optional?
-# TODO test
-# TODO adapt log levels
+
+__init_musicbrainzngs()
 
 
 def fetch_with_disc_id(disc_id, device):
-    global MUSICBRAINZNGS_INITIALIZED
-    if not MUSICBRAINZNGS_INITIALIZED:
-        version = main.exaile().get_user_agent_for_musicbrainz()
-        musicbrainzngs.set_useragent(*version)
-        MUSICBRAINZNGS_INITIALIZED = True
-
     musicbrainz_data = musicbrainzngs.get_releases_by_discid(
-        disc_id.id, toc=disc_id.toc_string, includes=["artists", "recordings"])
+        disc_id.id, toc=disc_id.toc_string, includes=[
+            'artists', 'recordings', 'isrcs', 'artist-credits'])
     
     if musicbrainz_data.get('disc') is not None:  # preferred: good quality
         disc_tracks = __parse_musicbrainz_disc_data(musicbrainz_data['disc'], disc_id, device)
@@ -53,47 +66,57 @@ def fetch_with_disc_id(disc_id, device):
             return disc_tracks
     
     # no useful data returned
-    logger.info('Musicbrainz returned not useful data: %s', musicbrainz_data)
+    logger.info('Musicbrainz returned no useful data: %s', musicbrainz_data)
     return None
 
 
 def __parse_musicbrainz_cdstub_data(cdstub_data, disc_id, device):
-    # prepopulate from disc_id parser
+    """ Parses cdstub data into xl.trax.Track """
+    # populate from disc_id parser
     tracks = discid_parser.parse_disc(disc_id, device)
     
-    if cdstub_data['track-count']:
-        track_count = cdstub_data['track-count']
+    # See "def_cdstub"
+    # Unused: disambiguation, def_cdstub-attribute_extension, def_cdstub-element_extension
+    
+    track_count = cdstub_data.get('track-count')
+    if track_count is not None:
         if len(tracks) is not track_count:
-            logger.info('Track count mismatch: real disc has %i, musicbrainz says %i. Ignoring all data.',
+            logger.debug('Track count mismatch: real disc has %i, musicbrainz says %i. Ignoring all data.',
                   len(tracks), track_count)
             return None
     
+    album_tags = dict()
+    
+    if cdstub_data.get('id') is not None:
+        album_tags['__musicbrainz_cdstub_id'] = cdstub_data['id']
+    
     album_title = cdstub_data['title']
+    album_tags['album'] = album_title
     
     artist = cdstub_data.get('artist')
+    if artist is not None:
+        album_tags['albumartist'] = artist
+        album_tags['artist'] = artist
+    
     barcode = cdstub_data.get('barcode')
+    if barcode is not None:
+        album_tags['barcode'] = barcode
     
     track_list = cdstub_data.get('track-list')
     if track_list is not None:
         for i in range(0, len(tracks)):
+            # See "def_nonmb-track"
+            # unused: length
             track = tracks[i]
-            new_tags = dict()
-            new_tags['album'] = album_title
+            track_tags = album_tags.copy() 
             
-            if barcode is not None:
-                new_tags['barcode'] = barcode
-                
-            if artist is not None:
-                new_tags['artist'] = artist
-            track_title = track_list[i].get('title')
-            if track_title is not None:
-                new_tags['title'] = track_title
+            track_tags['title'] = track_list[i]['title']
             
-            if artist is not None:
-                new_tags['artist'] = artist
-                new_tags['albumartist'] = artist
+            track_artist = track_list[i].get('artist')
+            if track_artist is not None:
+                track_tags['artist'] = track_artist
             
-            track.set_tags(**new_tags)
+            track.set_tags(**track_tags)
     return tracks, album_title
 
 
@@ -112,7 +135,7 @@ def __parse_musicbrainz_disc_data(disc_data, disc_id, device):
     
     (release, medium) = __choose_release_and_medium(suitable_releases)
     
-    return __parse_from_disc_data(release, medium, device, disc_id)
+    return __parse_medium_from_disc_data(release, medium, device, disc_id)
 
 
 def __choose_release_and_medium(suitable_releases):
@@ -139,24 +162,28 @@ def __choose_release_and_medium(suitable_releases):
 
 def __check_single_release(single_release, disc_id):
     # https://wiki.musicbrainz.org/Recording
-    
+
     priority = 0
-    
     # See https://wiki.musicbrainz.org/Barcode
-    if single_release.get('barcode') is not None and disc_id.mcn is not None:
-        mb_barcode = single_release['barcode'].lstrip('0')
+    mb_barcode = single_release.get('barcode')
+    if mb_barcode is not None and disc_id.mcn is not None:
+        mb_barcode = mb_barcode.lstrip('0')
         disc_barcode = disc_id.mcn.lstrip('0')
         
         if len(disc_barcode) > 5:  # empty string indicates missing barcode
             if mb_barcode == disc_barcode:
-                logger.info('Found exact barcode match: %s', disc_barcode)
+                logger.debug('Found exact barcode match: %s', disc_barcode)
                 priority = 100
             else:  # could be a different barcode format or a weak match
-                logger.info('Barcode mismatch: real disc has %s, musicbrainz says %s.',
+                logger.debug('Barcode mismatch: real disc has %s, musicbrainz says %s.',
                             disc_barcode, mb_barcode)
                 priority = -19
     
-    medium_list = single_release['medium-list']
+    medium_list = single_release.get('medium-list')
+    if medium_list is None:
+        logger.debug('Release contains no `medium-list`. This sounds fishy, ignoring.')
+        return None
+
     suitable_media = []
     for single_medium in medium_list:
         medium_priority = __check_single_medium(single_medium, disc_id)
@@ -173,45 +200,52 @@ def __check_single_release(single_release, disc_id):
 def __check_single_medium(single_medium, disc_id):
     # https://wiki.musicbrainz.org/Medium
     priority = 0
-    if single_medium.get('format') is not None:
-        if not 'CD' == single_medium['format']:
-            logger.info('Medium format mismatch: real disc is CD, musicbrainz says %s. Ignoring.',
-                  single_medium['format'])
+    format = single_medium.get('format')
+    if format is not None:
+        if not format == 'CD':
+            logger.debug('Medium format mismatch: real disc is CD, musicbrainz says %s. Ignoring.',
+                  format)
             return None
         priority = priority + 7
     
-    if single_medium.get('track-count') is not None:
-        if not single_medium['track-count'] == len(disc_id.tracks):
-            logger.info('Track count mismatch: real disc has %i, musicbrainz says %i. Ignoring.',
-                  len(disc_id.tracks), single_medium['track-count'])
+    track_count = single_medium.get('track-count')
+    if track_count is not None:
+        if not track_count == len(disc_id.tracks):
+            logger.debug('Track count mismatch: real disc has %i, musicbrainz says %i. Ignoring.',
+                  len(disc_id.tracks), track_count)
             return None
         priority = priority + 2
     
-    if not len(single_medium['track-list']) == len(disc_id.tracks):
-        logger.info('Track number mismatch: real disc has %i, musicbrainz says %i. Ignoring.',
-                  len(disc_id.tracks), single_medium['track-count'])
+    track_list = single_medium.get('track-list')
+    if track_list is None:
+        logger.debug('Medium contains no `track-list`. This sounds fishy, ignoring.')
+    if not len(track_list) == len(disc_id.tracks):
+        logger.debug('Track number mismatch: real disc has %i, musicbrainz says %i. Ignoring.',
+                  len(disc_id.tracks), track_list)
         return None
 
     # disc-list does not seem to contain important information.
 
     priority = 0
-    for i in range(0, len(single_medium['track-list'])):
-        single_mb_track = single_medium['track-list'][i]
+    for i in range(0, len(track_list)):
+        single_mb_track = track_list[i]
         single_disc_track = disc_id.tracks[i]
         track_prio = __check_single_track(single_mb_track, single_disc_track)
         if not track_prio:
-            logger.info('Track mismatch, ignoring this disk.')
+            logger.debug('Track mismatch, ignoring this disk.')
             return None
         priority = priority + track_prio
     return priority
 
 
 def __check_single_track(single_mb_track, single_disc_track):
+    # See "def_track-data"
     priority = 0
     
     # TODO: position or number?
-    if single_mb_track.get('position') is not None:
-        mb_position = int(single_mb_track['position'])
+    position = single_mb_track.get('position')
+    if position is not None:
+        mb_position = int(position)
         if mb_position == single_disc_track.number:
             priority = priority + 9
         else:
@@ -234,54 +268,161 @@ def __check_single_track(single_mb_track, single_disc_track):
         real_length_ms = single_disc_track.sectors * 1000 / 75
         len_diff = abs(real_length_ms - mb_length_ms)
         if len_diff > 10000:  # up to 5 seconds fade in / fade out time at start and end
-            logger.info('Massive track length mismatch: real disc has %ims, musicbrainz says %ims.',
+            logger.debug('Massive track length mismatch: real disc has %ims, musicbrainz says %ims.',
                         real_length_ms, mb_length_ms)
             return None
-        elif len_diff < 20:  # sector length = 1000ms/75 = 13+1/3
-            # exact track length match!
-            priority = priority + 50
+        elif len_diff < 20:  # sector length = 1000ms/75 = (13+1/3)ms
+            priority = priority + 50  # priority bonus for "exact" length match
         else:
-            print(len_diff)
             priority = priority - len_diff/100
-            logger.info('Slight track length mismatch: real disc has %i, musicbrainz says %i.',
+            logger.debug('Slight track length mismatch: real disc has %i, musicbrainz says %i.',
                         real_length_ms, mb_length_ms)
     
-    # TODO read and compare isrc? Musicbrainz does not seem to support that.
+    if single_mb_track.get('recording') is not None:
+        resource_priority = __check_single_track_as_recording(
+            single_mb_track.get('recording'), single_disc_track)
+        if resource_priority is None:
+            return None
+        else:
+            priority = priority + resource_priority
     
     return priority
 
 
-def __parse_from_disc_data(release, medium, device, disk_id):
-    # prepopulate from disc_id parser
+def __check_single_track_as_recording(mb_recording, disc_track):
+    # See "def_recording-element"
+
+    priority = 0
+    if hasattr(disc_track, 'isrc') and disc_track.isrc is not None:
+        mb_isrc_list = mb_recording.get('isrc-list')
+        if mb_isrc_list is not None:
+            if disc_track.isrc in mb_isrc_list:
+                priority = +1000
+            else:
+                priority = -100
+    return priority
+
+
+def __parse_medium_from_disc_data(release, medium, device, disc_id):
+    # populate from disc_id parser
     tracks = discid_parser.parse_disc(disc_id, device)
+    medium_tags = dict()
     
-    artist = release['artist-credit-phrase']
-    album_title = release['title']
-    date = release['date']
-    track_list = medium['track-list']
-    disc_number = '{0}/{1}'.format(medium['position'], 1)  # TODO calculate disc number?
+    disc_number = __get_disc_number(medium, release)
+    if disc_number is not None:
+        medium_tags['discnumber'] = disc_number
     
-    track_count = medium['track-count']
-    for track_index in range(0, track_count):
-        track = tracks[track_index]
-        #track_number = '{0}/{1}'.format(
-        #    track_list[track_index]['number'],  # TODO or position?
-        #    medium['track-count'])
-        track.set_tags(
-            artist=artist,
-            title=track_list[track_index]['recording']['title'],
-            albumartist=artist,
-            album=album_title,
-            #tracknumber=track_number,
-            discnumber=disc_number,
-            date=date,
-            #__length=int(track_list[track_index]['length']) / 1000,
-            # or track_list[track_index]['recording']['length']
-            # or track_list[track_index]['track_or_recording_length']
-            # TODO Get more data with secondary query, e.g. genre ("tags")? 
-            # https://wiki.musicbrainz.org/Genre
-            )
-        tracks.append(track)
+    if release.get('id') is not None:
+        medium_tags['__release_id'] = release['id']
+
+    if release.get('date') is not None:
+        medium_tags['date'] = release['date']
+
+    # TODO difference of medium.title vs. release.title?
+    album_title = medium.get('title')
+    if album_title is not None:
+        medium_tags['album'] = album_title
+    else:
+        album_title = release.get('title')
+        if album_title is not None:
+            medium_tags['album'] = album_title
+
+    artist = release.get('artist-credit-phrase')
+    if artist is not None:
+        medium_tags['albumartist'] = artist
+        medium_tags['artist'] = artist
+    
+    if release.get('barcode') is not None:
+        medium_tags['barcode'] = release.get('barcode')
+
+    mb_tracks = medium['track-list']
+    for track_index in range(0, len(mb_tracks)):
+        __parse_track_from_disc_data(
+            medium_tags.copy(), tracks[track_index], mb_tracks[track_index])
+
+    # needs the 'musicbrainz_albumid' tag to be set, thus this happens after writing track tags
+    __get_cover_image(release, tracks)
     
     return (tracks, album_title)
+
+
+def __get_cover_image(release, tracks):
+    """
+        Fetch a cover using musicbrainzcover's cover provider.
+        Requires the 'musicbrainz_albumid' tag to be set on the tracks.
+        
+        Documentation:
+        * https://musicbrainz.org/doc/Cover_Art_Archive/API
+    """
+    if release.get('id') is None:
+        return None
+    
+    try:
+        import plugins.musicbrainzcovers as musicbrainzcovers
+    except ImportError:
+        logger.warn('Cannot load musicbrainzcovers, will not fetch covers.')
+        return None
+    
+    cover_searcher = musicbrainzcovers.MusicBrainzCoverSearch(main.exaile())
+    cover_data = None
+    for resolution in ['500', '1200', '250']:
+        try:
+            db_string = '%s:%s' % (release['id'], resolution)
+            cover_data = cover_searcher.get_cover_data(db_string)
+            break
+        except:
+            logger.debug('Cannot fetch cover for %s in resolution %s', 
+                        release['id'], resolution)
+
+    if cover_data is not None:
+        for track in tracks:
+            db_string = 'musicbrainz:bar'
+            CoverManager.set_cover(track, db_string, cover_data)
+
+
+def __parse_track_from_disc_data(track_tags, xl_track, mb_track):
+    # See "def_track-data" in https://github.com/metabrainz/mmd-schema/blob/master/schema/musicbrainz_mmd-2.0.rng
+    # unused and populated from discid: position, number, length
+    
+    if mb_track.get('id') is not None:
+        track_tags['__musicbrainz_track_id'] = mb_track['id']
+    
+    if mb_track.get('title') is not None:
+        track_tags['title'] = mb_track['title']
+    
+    track_artist = mb_track.get('artist-credit-phrase')
+    if track_artist is not None:
+        track_tags['artist'] = track_artist
+    
+    if mb_track.get('recording') is not None:
+        mb_recording = mb_track['recording']
+        # see "def_recording-element"
+        # unused fields: length, annotation, disambiguation, ...
+        
+        recording_title = mb_recording.get('title')
+        if recording_title is not None and track_tags.get('title') is None:
+            track_tags['title'] = recording_title
+        
+        recording_id = mb_recording.get('id')
+        if recording_id is not None:
+            track_tags['musicbrainz_albumid'] = recording_id
+        
+    xl_track.set_tags(**track_tags)
+
+
+def __get_disc_number(medium, release):
+    """ Helper function to extract disc number string, e.g. 1/2 for the first of 2 CDs """
+    # We may also be able to use the 'disc-count' field of "medium"
+    medium_position = medium.get('position')
+    medium_count = release.get('medium-count')
+    if medium_count is None:
+        medium_count = len(release['medium-list'])
+    if medium_position is not None:
+        if medium_count is not None:
+            return '{0}/{1}'.format(medium_position, medium_count)
+        else:
+            return medium_position
+    elif medium_count is not None and medium_count == 1:
+        return '1/1'
+    return None
 

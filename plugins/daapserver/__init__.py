5a84a6ce7c5211e05cee7138156ac3c80c71ff5c
Rok Mandeljc <rok.mandeljc@gmail.com>
2019-09-27 23:55:15 +0200

    plugins/daapserver: fixed byte-string merging
    When merging list of byte-strings, the delimiter string needs to
    be a byte-string as well.

--- plugins/daapserver/__init__.py
+++ plugins/daapserver/__init__.py
@@ -24,9 +24,9 @@ class CollectionWrapper:
             if self.daap is None:
                 do = self.parser.parse(self.track)[0]
                 if do is not None:
-                    self.daap = ''.join([d.encode() for d in do])
+                    self.daap = b''.join([d.encode() for d in do])
                 else:
-                    self.daap = ''
+                    self.daap = b''
             return self.daap
 
         def get_original_filename(self):

6d6878251150d342559bbe76c072a65281f60e3f
Rok Mandeljc <rok.mandeljc@gmail.com>
2020-04-15 13:11:40 +0200

    ipython_view.py: do not use autoedit_syntax
    It was removed in IPython 5.x, and had apparently been broken for
    many years before that.

--- plugins/ipconsole/ipython_view.py
+++ plugins/ipconsole/ipython_view.py
@@ -225,8 +225,6 @@ class IterableIPShell:
                 self.IP.input_splitter.push(line)
                 self.iter_more = self.IP.input_splitter.push_accepts_more()
             self.prompt = self.generatePrompt(self.iter_more)
-            if self.IP.SyntaxTB.last_syntax_error and self.IP.autoedit_syntax:
-                self.IP.edit_syntax_error()
             if not self.iter_more:
                 if self.no_input_splitter:
                     source_raw = '\n'.join(self.lines)

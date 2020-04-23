bcbac205636ff260bf819a67c3fdd6d2e4ecbacb
Johannes Sasongko <sasongko@gmail.com>
2020-04-23 23:10:54 +1000

    xlgui/devices: Fix device manager not opening
    Fixes: https://github.com/exaile/exaile/issues/699

--- xlgui/devices.py
+++ xlgui/devices.py
@@ -42,7 +42,7 @@ class ManagerDialog(Gtk.Window):
 
     __gtype_name__ = 'DeviceManager'
 
-    tree_devices, model = GtkTemplate.Child.widgets(5)
+    tree_devices, model = GtkTemplate.Child.widgets(2)
 
     def __init__(self, parent, main):
         Gtk.Window.__init__(self)

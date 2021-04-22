1d23c465ee4d47d46c10fc2bdce4ab98d3c9b2b2
luzip665 <andre@tango-erfurt.de>
2021-04-23 00:12:08 +0200

    First Button

--- plugins/quickbuttons/__init__.py
+++ plugins/quickbuttons/__init__.py
@@ -1,67 +1,69 @@
 
 from gi.repository import Gtk
-from xl import common, event
+from xl import event, settings
 
 class QuickButtons:
 
     def enable(self, exaile):
         self._exaile = exaile
+        self._options = {
+            'playlist/enqueue_by_default': {'value': None, 'widget': None, 'type': 'toggle', 'label': 'enq'},
+            'player/auto_advance_delay': {'value': None, 'widget': None, 'type': 'spin', 'label': 'delay'},
+            'queue/disable_new_track_when_playing': {'value': None, 'widget': None, 'type': 'toggle', 'label': 'dis'},
+            'queue/remove_item_when_played': {'value': None, 'widget': None, 'type': 'toggle', 'label': 'rem'},
+            'player/auto_advance': {'value': None, 'widget': None, 'type': 'toggle', 'label': 'auto'}
+        }
 
+        event.add_callback(self._on_option_set, 'playlist_option_set')
+        event.add_callback(self._on_option_set, 'queue_option_set')
+        event.add_callback(self._on_option_set, 'player_option_set')
 
     def disable(self, exaile):
         pass
 
     def _on_option_set(self, event_name, event_source, option):
-        print('option set')
+        if not option in self._options:
+            return
 
-        self._enqueue_by_default = event_source.get_option(option)
+        self._options[option]['value'] = settings.get_option(option)
+        if self._options[option]['type'] == 'toggle':
+            self._options[option]['widget'].set_active(self._options[option]['value'])
+        if self._options[option]['type'] == 'spin':
+            self._options[option]['widget'].get_children()[0].set_value(self._options[option]['value'])
 
-        pass
+    def _on_toggle(self, widget, setting):
+        settings.set_option(setting, widget.get_active())
+
+    def _add_button(self, setting):
+        if self._options[setting]['type'] == 'toggle':
+            tb = Gtk.ToggleToolButton()
+            tb.set_label(self._options[setting]['label'])
+            tb.set_active(self._options[setting]['value'])
+            tb.connect('toggled', self._on_toggle, setting)
+
+        if self._options[setting]['type'] == 'spin':
+            tbs = Gtk.SpinButton()
+            tbs.set_adjustment(Gtk.Adjustment(value=float(self._options[setting]['value']), lower=0, upper=10, step_incr=1, page_incr=1, page_size=0))
+            tbs.show()
+            tb = Gtk.ToolItem()
+            tb.add(tbs)
+
+        self._options[setting]['widget'] = tb
+        tb.show()
+        self._toolbar.insert(tb, -1)
 
     def on_gui_loaded(self):
 
-        self._enqueue_by_default = None
-        name = 'playlist'
-        options = {'playlist/enqueue_by_default' : '_enqueue_by_default'}
+        self._status_bar = self._exaile.gui.builder.get_object('status_bar')
+        self._toolbar = Gtk.Toolbar()
 
-        self._settings_unsub = common.subscribe_for_settings(name, options, self)
-        event.add_callback(self._on_option_set, 'playlist_option_set')
+        for k in self._options:
+            self._options[k]['value'] = settings.get_option(k)
+            self._add_button(k)
 
-
-        status_bar = self._exaile.gui.builder.get_object('status_bar')
-        area = Gtk.Toolbar()
-
-        # toggle button
-        newtb = Gtk.ToggleToolButton()
-        newtb.set_label('test')
-        newtb.show()
-        newtb.set_active(self._enqueue_by_default)
-        area.insert(newtb, 0)
-
-        # spinner
-        spinbtn = Gtk.SpinButton()
-        spinbtn.set_adjustment(Gtk.Adjustment(value=0, lower=0, upper=10, step_incr=1, page_incr=1, page_size=0))
-        spinbtn.show()
-        spinbtn_toolitem = Gtk.ToolItem()
-        spinbtn_toolitem.add(spinbtn)
-        spinbtn_toolitem.show()
-        area.insert(spinbtn_toolitem, 1)
-
-        '''
-        Buttons
-        
-    Queue tracks by default instead of playing them
-    Disallow playing new tracks when another track is playing
-    Remove track from queue upon playback
-    Automatically advance to the next track
-    Delay between tracks (ms)
-
-        '''
-
-        area.show()
-
-        status_bar.pack_start(area, False, True, 0)
-        status_bar.reorder_child(area, 0)
+        self._toolbar.show()
+        self._status_bar.pack_start(self._toolbar, False, True, 0)
+        self._status_bar.reorder_child(self._toolbar, 0)
 
     def on_exaile_loaded(self):
         pass

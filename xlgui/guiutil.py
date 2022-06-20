exaile/exaile

commit dad5492f1ccc72b8786cd5200adb6f220eb51233

Translation on windows * Setting os.environ['lang'] to locale.getlocale on win32 * Load Gtk.Builder with translation in guiutil.get_builder * Adding new cli-option --use-lang to set custom language

xlgui/guiutil.py
@@ -27,6 +27,7 @@
 import contextlib
 import logging
 import os.path
+import sys
 
 from gi.repository import Gio
 from gi.repository import Gdk
@@ -36,6 +37,7 @@ from gi.repository import Gtk
 from gi.repository import Pango
 
 from xl import settings, xdg
+from xl.nls import gettext as _
 
 # Required for xlgui.get_controller()
 import xlgui
@@ -728,6 +730,43 @@ def without_model(tv):
 
         if scroll_to:
             tv.scroll_to_cell(scroll_to, None, True, 0, 0)
+
+
+def get_builder(uifile: str) -> Gtk.Builder:
+    """
+    Loads Gtk.Builder with uifile.
+    In case of platform win32 each translatable node in the ui is translated here
+
+    Relates to https://gitlab.gnome.org/GNOME/pygobject/-/issues/422
+
+    :param uifile: Path to uifile
+    """
+    builder = Gtk.Builder()
+
+    if sys.platform == 'win32':
+        with open(uifile, 'r') as fp:
+            template = fp.read()
+            template_string = get_template_translated(template)
+            builder.add_from_string(template_string)
+
+    else:
+        builder.add_from_file(uifile)
+
+    return builder
+
+
+def get_template_translated(template_ui_xml: str) -> str:
+    """
+    Insert translations into template_ui_xml for each translatable element
+    """
+    import xml.etree.ElementTree as ET
+
+    tree = ET.fromstring(template_ui_xml)
+    for node in tree.iter():
+        if 'translatable' in node.attrib:
+            node.text = _(node.text)
+    xml_text = ET.tostring(tree, encoding='unicode', method='xml')
+    return xml_text
 
 
 # vim: et sts=4 sw=4

commit 373b0fe092cb6b8fe54012d207c83a60515ff388

guiutil.get_builder: Read GtkBuilder file as binary
This code path only affects Windows.

Previously the XML file was being read into a string, using some default
encoding Python chooses. This caused errors in some circumstances, for
example when trying to show the ReplayGain preferences page.

This bug was introduced in dad5492f1ccc72b8786cd5200adb6f220eb51233 , which
added a hack to translate GtkBuilder strings on Windows.

xlgui/guiutil.py
@@ -28,6 +28,7 @@ import contextlib
 import logging
 import os.path
 import sys
+from typing import Union
 
 from gi.repository import Gio
 from gi.repository import Gdk
@@ -744,7 +745,7 @@ def get_builder(uifile: str) -> Gtk.Builder:
     builder = Gtk.Builder()
 
     if sys.platform == 'win32':
-        with open(uifile, 'r') as fp:
+        with open(uifile, 'rb') as fp:
             template = fp.read()
             template_string = get_template_translated(template)
             builder.add_from_string(template_string)
@@ -755,7 +756,7 @@ def get_builder(uifile: str) -> Gtk.Builder:
     return builder
 
 
-def get_template_translated(template_ui_xml: str) -> str:
+def get_template_translated(template_ui_xml: Union[str, bytes]) -> str:
     """
     Insert translations into template_ui_xml for each translatable element
     """
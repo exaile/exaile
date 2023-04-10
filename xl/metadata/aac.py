e16fa3f83afb003845fe041509cdf44b668235c6
luzip665 <82048917+luzip665@users.noreply.github.com>
2023-04-10 23:21:26 +0200

    Add support for aac tagging with id3format (#852)

---
+++ xl/metadata/aac.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2023 luzip665
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+#
+# The developers of the Exaile media player hereby grant permission
+# for non-GPL compatible GStreamer and Exaile plugins to be used and
+# distributed together with GStreamer and Exaile. This permission is
+# above and beyond the permissions granted by the GPL license by which
+# Exaile is covered. If you modify this code, you may extend this
+# exception to your version of the code, but you are not obligated to
+# do so. If you do not wish to do so, delete this exception statement
+# from your version.
+import logging
+
+logger = logging.getLogger(__name__)
+
+from xl.metadata._id3 import ID3Format
+from mutagen import id3
+
+
+class AACFormat(ID3Format):
+    MutagenType = id3.ID3FileType
+    pass

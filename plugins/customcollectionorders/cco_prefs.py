5529f9a3845cf78ff85281e152b4e85c510c53c1
luzip665 <82048917+luzip665@users.noreply.github.com>
2025-01-02 21:52:52 +0100

    Custom collection order - improvements (#951)
    Add icon button for editing
    Strip tags in levels and display
    Changing some labels

--- plugins/customcollectionorders/cco_prefs.py
+++ plugins/customcollectionorders/cco_prefs.py
@@ -1,5 +1,6 @@
 import os, json
 from gi.repository import Gtk
+from gi.repository import GLib
 
 from xl.nls import gettext as _
 from xl.metadata import tags
@@ -18,23 +19,37 @@ def init(dialog, builder):
 class eco_prefs:
     def __init__(self, dialog, builder):
         self.builder = builder
-        self.parent = dialog.window
+        self.parent = dialog.parent
 
         self.grid = self.builder.get_object('preferences_pane')
         self.model = self.builder.get_object('model')
         self.list = self.builder.get_object('orders_tree')
 
-        """Show trash can"""
+        self.list.connect("row-activated", self._on_row_activated)
+        self.builder.connect_signals(self)
+
+        name_column = builder.get_object('name_column')
+
+        """Name column"""
+        name_cellrenderer = Gtk.CellRendererText()
+        name_column.pack_start(name_cellrenderer, True)
+        name_column.add_attribute(name_cellrenderer, 'text', 0)
+
+        """Edit button"""
+        edit_cellrenderer = common.ClickableCellRendererPixbuf()
+        edit_cellrenderer.props.icon_name = 'edit'
+        edit_cellrenderer.props.xalign = 1
+        edit_cellrenderer.props.width = 30
+        edit_cellrenderer.connect('clicked', self._on_edit_cellrenderer_clicked)
+        name_column.pack_start(edit_cellrenderer, False)
+
+        """Trash can"""
         remove_cellrenderer = common.ClickableCellRendererPixbuf()
         remove_cellrenderer.props.icon_name = 'edit-delete'
         remove_cellrenderer.props.xalign = 1
+        remove_cellrenderer.props.width = 30
         remove_cellrenderer.connect('clicked', self._on_remove_cellrenderer_clicked)
-
-        name_column = builder.get_object('name_column')
-        name_column.pack_start(remove_cellrenderer, True)
-
-        self.list.connect("row-activated", self._on_row_activated)
-        self.builder.connect_signals(self)
+        name_column.pack_start(remove_cellrenderer, False)
 
         self.custom_orders = custom_orders()
 
@@ -58,9 +73,16 @@ class eco_prefs:
         order_number = self.model[path][1]
         self._order_edit(order_number)
 
-    def _on_remove_cellrenderer_clicked(self, cellrenderer, path):
+    def _on_edit_cellrenderer_clicked(self, cellrenderer, path: str):
         order_number = self.model[path][1]
-        check = dialogs.yesno(self.parent, _('really delete?'))
+        GLib.idle_add(self._order_edit, order_number)
+
+    def _on_remove_cellrenderer_clicked(self, cellrenderer, path: str):
+        order_number = self.model[path][1]
+        GLib.idle_add(self._ask_for_delete_order, order_number)
+
+    def _ask_for_delete_order(self, order_number: int):
+        check = dialogs.yesno(self.parent, _('Really delete this order?'))
         if check != Gtk.ResponseType.YES:
             return
         del self.custom_orders[order_number]
@@ -85,7 +107,7 @@ class eco_prefs:
                 )
 
         tree_level_hint = _(
-            'Comma separated list of the nodes in the tree view. Right now it\'s not possible to use more than one tag per level.\n'
+            'Comma separated list of tags to use as nodes in the tree view. Right now it\'s not possible to use more than one tag per node.\n'
             'Every tag can be used.'
         )
         display_hint = _(

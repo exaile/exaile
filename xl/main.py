b503b4a5b500a8af20a3844ab308b7a49e681b75
Johannes Sasongko <johannes@sasongko.org>
2025-03-25 14:50:14 +1100

    plugins: Support GIRepository-3.0
    GLib 2.84 integrates GIRepository (as `-3.0`). Rather than checking the
    GLib version for the right GIRepository version to `require_version`, this
    commit simply uses a try-except.
    
    This also moves the `require_version` call from main.py to plugins.py,
    where the GIRepository import actually is.
    
    Fixes: https://github.com/exaile/exaile/issues/962

--- xl/main.py
+++ xl/main.py
@@ -54,7 +54,6 @@ def _do_heavy_imports():
     gi.require_version('Gdk', '3.0')
     gi.require_version('Gtk', '3.0')
     gi.require_version('Gst', '1.0')
-    gi.require_version('GIRepository', '2.0')
     gi.require_version('GstPbutils', '1.0')
 
     from gi.repository import GLib, Gio, Gtk

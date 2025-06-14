9ec74130a4a8527af10b6045ecfc87eb15b6dc0f
luzip665 <luzip665@posteo.de>
2025-06-14 23:55:27 +0200

    playlist export * use paths instead of uris for local files

--- tests/xl/test_FormatConverter.py
+++ tests/xl/test_FormatConverter.py
@@ -11,36 +11,42 @@ class Test_FormatConverter:
     TIMEOUT = 2000
     MAX_ENTRIES = 2
 
-    def test_relative(self):
+    def test_get_track_export_path_relative(self):
         fc = FormatConverter('test')
         peo = PlaylistExportOptions
         peo.relative = True
+
         test = fc.get_track_export_path(
             'file:///tmp/playlist', 'file:///tmp/track1', peo
         )
         assert test == 'track1'
+
         test = fc.get_track_export_path(
             'file:///tmp/playlist', 'file:///tmp123/track1', peo
         )
         assert test == '../tmp123/track1'
+
         test = fc.get_track_export_path(
             'file:///tmp/playlist', 'file:///tmp123/track1%202', peo
         )
         assert test == '../tmp123/track1 2'
 
-    def test_stream(self):
+    def test_track_export_path_stream(self):
         fc = FormatConverter('test')
+
         test = fc.get_track_export_path(
             'file:///tmp/playlist', 'http://ec3.yesstreaming.net:3420/stream', None
         )
         assert test == 'http://ec3.yesstreaming.net:3420/stream'
 
-    def test_absolute(self):
+    def test_track_export_path_absolute(self):
         fc = FormatConverter('test')
+
         test = fc.get_track_export_path(
             'file:///tmp/playlist', 'file:///tmp/track1', None
         )
         assert test == '/tmp/track1'
+
         test = fc.get_track_export_path(
             'file:///tmp/playlist', 'file:///tmp123/track1%202', None
         )
